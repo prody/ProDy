f935e932e2aad566b6a64261dad2339f9603f7a1
Ahmet Bakan <lordnapi@gmail.com>
2010-11-02 21:38:50 -0400

    setup.py added to repo.

--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 from distutils.core import setup
-setup(name='prody',
-      version='0.0.2',
-      description='Protein Structure and Dynamics',
+setup(name='ProDy',
+      version='0.1.0a',
+      description='Protein Dynamics Analysis',
       long_description='A Python Package for Protein Structural Dynamics Analysis',
       author='Ahmet Bakan',
       author_email='ahb12@pitt.edu',
       url='http://www.pitt.edu/~ahb12/python/prody',
       packages=['prody', 'prody.dynamics', 'prody.proteins'],
-      license='GPL',
-      keywords=('protein, dynamics, elastic network model, gaussian network model'
+      license='GPLv3',
+      keywords=('protein, dynamics, elastic network model, gaussian network model, '
                 'anisotropic network model, essential dynamics analysis, '
-                'principal component analysis, ProteinDataBank, PDB', 
-                'GNM', 'ANM', 'PCA')
+                'principal component analysis, ProteinDataBank, PDB, GNM, ANM, PCA')
       )
