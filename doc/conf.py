9d72a4fbdf69ba9d56fad15b82b01e94afe97c31
Ahmet Bakan <lordnapi@gmail.com>
2011-10-14 10:42:20 -0400

    Incremented version number.

--- doc/conf.py
+++ doc/conf.py
@@ -65,7 +65,7 @@ def getRevisionNumber():
     logs = logfile.read()
     logfile.close()
     return str(logs.count('Author:'))
-version = '0.8.1'
+version = '0.8.2'
 # The full version, including alpha/beta/rc tags.
 release =  version
 
