0a5bf37dd540b6ad4708ab59c533da3220afadc1
Ahmet Bakan <lordnapi@gmail.com>
2011-10-21 15:46:48 -0400

    Update test package to use nose when available.

--- prody/tests/__init__.py
+++ prody/tests/__init__.py
@@ -17,20 +17,44 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+"""ProDy test suite.  Usage::
+
+  import prody.tests
+  prody.tests.test()
+
+
+Testing will use :mod:`nose` if it is available, otherwise it will use 
+:mod:`unittest`.
+
+"""
+
 __author__ = 'Ahmet Bakan'
 __copyright__ = 'Copyright (C) 2010-2011 Ahmet Bakan'
 
 import sys
-import os.path
-import unittest
-import test_proteins
-import test_select
-import test_ensemble
+import prody
 
-def test(verbosity=2, descriptions=True, stream=sys.stderr):
-    testrunner = unittest.TextTestRunner(stream, descriptions, verbosity)
-    for module in [test_proteins, test_select, test_ensemble]:
-        testrunner.run(unittest.defaultTestLoader.loadTestsFromModule(module))
+try:
+    import nose
     
+except ImportError:
+    LOGGER.warning('Failed to import nose, using unittest for testing.')
+    LOGGER.info('nose is available at http://readthedocs.org/docs/nose/')
+    
+    if sys.version_info[:2] > (2,6):
+        def test(verbosity=2, descriptions=True, stream=sys.stderr):
+            testrunner = unittest.TextTestRunner(stream, descriptions, 
+                                                 verbosity)
+            for module in ['test_dynamics', 'test_proteins', 'test_select', 
+                           'test_ensemble']:
+                testrunner.run(unittest.defaultTestLoader.
+                               loadTestsFromModule(__import__(module)))
+    else:
+        LOGGER.warning('Unit tests are compatible with Python 2.7 and later.')
+
+else:
+    from numpy.testing import Tester
+    test = Tester().test
+
 if __name__ == '__main__':
     test()
