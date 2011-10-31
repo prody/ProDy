fcb5c411c6c074c4cf9e410a600b9e1269e86b26
Ahmet Bakan <lordnapi@gmail.com>
2011-10-31 11:50:31 -0400

    Changed version number in NMWiz installer.

--- plugins/nmwiz_install.py
+++ plugins/nmwiz_install.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-__version__ = '0.8.1'
+__version__ = '1.0'
 
 import os
 import os.path
