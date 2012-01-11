0df07e6bb5ba2438e8775eeb88eca8a441e0ca25
Ahmet Bakan <lordnapi@gmail.com>
2012-01-11 11:10:51 -0500

    Updated copyright info.

--- prody/tests/test_proteins.py
+++ prody/tests/test_proteins.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # ProDy: A Python Package for Protein Dynamics Analysis
 # 
-# Copyright (C) 2010-2011 Ahmet Bakan
+# Copyright (C) 2010-2012 Ahmet Bakan
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 """This module contains unit tests for :mod:`~prody.proteins`."""
 
 __author__ = 'Ahmet Bakan'
-__copyright__ = 'Copyright (C) 2010-2011 Ahmet Bakan'
+__copyright__ = 'Copyright (C) 2010-2012 Ahmet Bakan'
 
 import os
 
