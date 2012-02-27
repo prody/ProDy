a72c5a5b7d4bb675ded020dd52d4b24a09d803df
Ahmet Bakan <lordnapi@gmail.com>
2012-02-27 11:45:28 -0500

    Updated code to update commands docs.

--- doc/commands/commands.py
+++ doc/commands/commands.py
@@ -2,13 +2,47 @@
 __author__ = 'Ahmet Bakan'
 __copyright__ = 'Copyright (C) 2010-2011 Ahmet Bakan'
 
-from os import system
-from glob import glob
 from os import path
+from glob import glob
+from subprocess import Popen, PIPE
 
+import prody
 from prody import routines
 
-system('prody > prody.txt')
-for cmd in routines.commands.choices.keys(): 
-    system('prody ' + cmd + ' -h > ' + 'prody_' + cmd + '.txt')
-    system('prody ' + cmd + ' --examples > ' + 'prody_' + cmd + '_eg.txt')
+#system('prody > prody.txt')
+for cmd in routines.PRODY_COMMANDS: 
+    
+    pkg = getattr(getattr(prody, 'routines'), 'prody_' + cmd)
+    
+    with open('prody_' + cmd + '.rst', 'w') as rst:
+        rst.write(""".. _prody-{cmd:s}:
+
+*******************************************************************************
+prody {cmd:s}
+*******************************************************************************
+
+{doc:s}
+
+Usage
+===============================================================================
+
+Running :command:`prody {cmd} -h` displays::
+
+""".format(cmd=cmd, doc=pkg.__doc__))
+    
+        pipe = Popen(['prody', cmd, '-h'], stdout=PIPE, stderr=PIPE)
+        for line in pipe.stdout.readlines():    
+            rst.write('  ' + line)
+
+        rst.write("""
+Examples
+===============================================================================
+
+Running :command:`prody {cmd:s} --examples` displays::
+
+""".format(cmd=cmd, doc='deneme'))
+    
+
+        pipe = Popen(['prody', cmd, '--examples'], stdout=PIPE, stderr=PIPE)
+        for line in pipe.stdout.readlines():    
+            rst.write('  ' + line)
