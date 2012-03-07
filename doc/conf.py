670be4ded09d445718a7942780371f4088ca9783
Ahmet Bakan <lordnapi@gmail.com>
2012-03-06 19:35:52 -0500

    Removed -dev from version number.

--- doc/conf.py
+++ doc/conf.py
@@ -65,7 +65,7 @@ def getRevisionNumber():
     logs = logfile.read()
     logfile.close()
     return str(logs.count('Author:'))
-version = '1.0-dev'
+version = '1.0'
 # The full version, including alpha/beta/rc tags.
 release =  version
 
