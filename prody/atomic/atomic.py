f10451ad5ea331fa1bf4b1edd2dfdfdf04f709d3
Ahmet Bakan <lordnapi@gmail.com>
2012-04-05 22:06:59 -0400

    Fixed a bug in evaluation of atomic attribtues.

--- prody/atomic/atomic.py
+++ prody/atomic/atomic.py
@@ -52,7 +52,7 @@ class Atomic(object):
             selstr = name
             items = name.split('_')
             word = items[0]
-            if (isKeyword(word) or items == 'not' or isMacro(word) or
+            if (isKeyword(word) or word == 'not' or isMacro(word) or
                 self.isData(word)):
                 selstr = ' '.join(items)
                 return SELECT.select(self, selstr)
