def997873e5c2812943d84f9260f8badcfdbee41
Ahmet Bakan <lordnapi@gmail.com>
2012-05-13 11:44:50 -0400

    Updated checkTypes function, refined checkCoords function, and added some unittests.

---
+++ prody/tests/test_utilities/test_checkers.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# ProDy: A Python Package for Protein Dynamics Analysis
+# 
+# Copyright (C) 2010-2012 Ahmet Bakan
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+"""This module contains unit tests for :mod:`prody.measure.measure` module."""
+
+__author__ = 'Ahmet Bakan'
+__copyright__ = 'Copyright (C) 2010-2012 Ahmet Bakan'
+
+import unittest
+
+from numpy.random import random
+from numpy.testing import assert_equal
+
+from prody.utilities import checkCoords, checkTypes
+
+COORDS = random((10, 3))*10
+COORDSET = random((2, 10, 3))*10
+
+class TestCheckCoords(unittest.TestCase):
+    
+    def testInvalidCoords(self):
+        
+        self.assertRaises(ValueError, checkCoords, [None])
+        
+    def testCoordsEquality(self):
+        
+        assert_equal(COORDS, checkCoords(COORDS))
+        
+    def testCoordsIdentity(self):
+        
+        self.assertIsNot(COORDS, checkCoords(COORDS))
+        
+        
+    def testCoordsetEquality(self):
+        
+        assert_equal(COORDSET, checkCoords(COORDSET, cset=True))
+        
+    def testCoordsetIdentity(self):
+        
+        self.assertIsNot(COORDSET, checkCoords(COORDSET, cset=True))
+
+
+class testCheckTypes(unittest.TestCase):
+    
+    def testCorrectMonotypeOneArg(self):
+        
+        self.assertTrue(checkTypes({'i': 1}, i=int))
+        
+    def testCorrectMonotypeTwoArgs(self):
+        
+        self.assertTrue(checkTypes({'i': 1, 'n': 10}, i=int, n=int))
+    
+    def testCorrectMultitypeOneArg(self):
+        
+        self.assertTrue(checkTypes({'i': 1.}, i=(int, float)))
+        
+    def testCorrectMonotypeTwoArgs(self):
+        
+        self.assertTrue(checkTypes({'i': 1, 'n': 10.}, i=(int, float), 
+                                                       n=(int, float)))
+                                                       
+    def testWrongMonotypeOneArg(self):
+        
+        self.assertRaises(TypeError, checkTypes, {'i': 1.0}, i=int)
+        
+    def testWrongMonotypeTwoArgs(self):
+        
+        self.assertRaises(TypeError, checkTypes, {'i': 1, 'n': 10}, 
+                                                  i=int, n=int)
+    
+    def testWrongMultitypeOneArg(self):
+        
+        self.assertRaises(TypeError, checkTypes, {'i': '1.'}, 
+                                      i=(int, float))
+        
+    def testWrongMonotypeTwoArgs(self):
+        
+        self.assertRaises(TypeError, checkTypes, {'i': 1, 'n': '10.'}, 
+                            i=(int, float), n=(int, float))
--- prody/utilities/checkers.py
+++ prody/utilities/checkers.py
@@ -20,136 +20,88 @@
 __author__ = 'Ahmet Bakan'
 __copyright__ = 'Copyright (C) 2010-2012 Ahmet Bakan'
 
-from numpy import ndarray
+from numpy import array
 
 __all__ = ['checkCoords', 'checkTypes']
 
 
-def checkCoords(array, name='array', cset=False, n_atoms=None, reshape=None, 
-                dtype=(float,)):
-    """Check whether *array* is a coordinate set.  An exception is raised if 
-    the array is not suitable.
+def checkCoords(coords, name='coords', cset=False, n_atoms=None, reshape=None, 
+                dtype=float):
+    """Return a copy of *coords* as a :class:`numpy.ndarray` instance after its
+    dimensionality and shape are checked for fitness to be a coordinate set.
     
-    :arg array: coordinate array
-    :type array: :class:`numpy.ndarray`
+    :arg coords: coordinate data, any array like object 
     
-    :arg name: argument name used in the function where array is being checked
-    :type name: str
+    :arg name: name of *coords* argument
     
-    :arg cset: whether the arra
-    :type cset: bool
+    :arg cset: allow multiple coordinate sets, default is **False**
     
-    :arg n_atoms: number of atoms, when available is used to check the shape of
-        the array
-    :type n_atoms: int
+    :arg n_atoms: number of atoms
     
-    :arg reshape: when *cset* is **True**, reshape array
-    :type reshape: bool
+    :arg reshape: reshape *coords* to multiple coordinate set  
     
-    :arg dtype: acceptable data types for array elements, default is 
-        :func:`float`
-    :type dtype: type
+    :arg dtype: allowed data types, default is :func:`float`
     
-    :returns: coordinate array is returned, after its shape or element data 
-        type is changed when needed"""
+    :returns: coordinate array
+    
+    .. note::  Note that the array that this function returns is not the array 
+        that is given as input.  This helps ProDy classes to internalize the
+        data.
+    """
 
-    assert isinstance(name, str), 'name must be a string'
-    assert isinstance(cset, bool), 'cset must be a boolean'
-    assert n_atoms is None or isinstance(n_atoms, int) and n_atoms >= 0, \
-        'n_atoms must be a positive integer'
-    assert reshape is None or isinstance(reshape, bool), \
-        'reshape must be a boolean'
     if not isinstance(dtype, tuple):
         dtype = (dtype, )
 
-    if not isinstance(array, ndarray):
-        raise TypeError(name + ' must be a Numpy array')
-        
-    elif cset and array.ndim not in (2,3): 
-        raise ValueError(name + '.ndim must be 2 or 3')
-        
-    elif not cset and array.ndim != 2:
-        raise ValueError(name + '.ndim must be 2')
-        
-    elif array.shape[-1] != 3:
-        raise ValueError(name + '.shape[-1] of 3, i.e. ([n_csets,]n_atoms,3)')
-        
-    if n_atoms is not None and n_atoms != 0 and array.shape[-2] != n_atoms:
-        raise ValueError(name + ' size do not match number of atoms')
-        
-    if array.dtype not in dtype:
-        try:
-            array = array.astype(dtype[0])
-        except ValueError:
-            raise ValueError(name + '.astype(' + str(dtype[0]) + ') fails, '
-                            'float type could not be assigned')
-                            
-    if cset and reshape and array.ndim == 2:
-        array = array.reshape([1, array.shape[0], 3])
-        
-    return array
-
-
-def checkTypes(**types):
-    """Decorate a function with a type checker.  In this decorator design, a 
-    single line needs to be added inside the function to make the call for
-    type checking.  
+    coords = array(coords, dtype=dtype[0])
+    ndim = coords.ndim
+    shape = coords.shape
     
-    >>> from prody.utilities import checkTypes
-    >>> @checkTypes(n=int, i=int)
-    ... def incr(n, i):
-    ...     '''Increment *n* by *i*, both arguments must be integers.'''
-    ...     
-    ...     globals()['incr'].check_types(locals())
-    ...     return n + i
-    >>> incr(10, 1)
-    11
-    >>> incr(10., 1)
-    Traceback (most recent call last):
-      File "checkers.py", line 81, in <module>
-        incr(10., 1)
-      File "checkers.py", line 78, in incr
-        globals()['incr'].func_typecheck(locals())
-      File "checkers.py", line 65, in func_typecheck
-        .format(arg, tstr, type(val).__name__))
-    TypeError: `n` must be int, not float"""
+    if cset and ndim not in (2,3): 
+        raise ValueError(str(name) + '.ndim must be 2 or 3')
+        
+    elif not cset and ndim != 2:
+        raise ValueError(str(name) + '.ndim must be 2')
+        
+    elif shape[-1] != 3:
+        raise ValueError(str(name) + '.shape[-1] must be 3')
+        
+    if n_atoms and shape[-2] != n_atoms:
+        raise ValueError(str(name) + ' size do not match number of atoms')
+        
+    if cset and reshape and ndim == 2:
+        coords = coords.reshape([1, shape[0], 3])
+        
+    return coords
 
-    def decorate(func):
+
+def checkTypes(args, **types):
+    """Return **True** if types of *args* match those given in *types*, 
+    otherwise raise a :class:`TypeError`.
     
-        def check_types(local, types=types):
+    ::
+        
+        def incr(n, i):
+            '''Return sum of *n* and *i*.'''
             
-            for arg, val in local.iteritems():
-                
-                if arg in types and not isinstance(val, types[arg]):
-                    
-                    tps = types[arg]
-                    if isinstance(tps, (list, tuple)):
-                        if len(tps) > 1:
-                            tstr = 'one of ' + ', '.join([tp.__name__ 
-                                                for tp in tps[:-1]]
-                                                ) + ', or ' + tps[-1].__name__
-                        
-                        else:
-                            tstr = tps[0].__name__
-                    
-                    else:
-                        tstr = tps.__name__
-                    
-                    raise TypeError('`{0:s}` must be an instance of {1:s}, '
-                            'not {2:s}'.format(arg, tstr, type(val).__name__))
-                
-        func.check_types = check_types
-        return func
-    return decorate 
-    
-    
-if __name__ == '__main__':
-
-    @checkTypes(n=int)
-    def incr(n, i):
-        '''Increment *n* by *i*, both arguments must be integers.'''
+            checkTypes(locals(), n=(float, int), i=(float, int))
+            return n + i"""
         
-        globals()['incr'].check_types(locals())
-        return n + i
-    incr(10, 1)
-    incr(10., 1)
+    for arg, allowed in types.iteritems():
+        if arg in args and not isinstance(args[arg], types[arg]):
+
+            val = args[arg]
+            if isinstance(allowed, (list, tuple)):
+                if len(allowed) > 1:
+                    tstr = ', '.join([repr(tp.__name__) for tp in allowed[:-1]]
+                                     ) + ', or ' + repr(allowed[-1].__name__)
+                
+                else:
+                    tstr = repr(allowed[0].__name__)
+            
+            else:
+                tstr = repr(allowed.__name__)
+            
+            raise TypeError('{0:s} must be an instance of {1:s}, not {2:s}'
+                            .format(repr(arg), tstr, repr(type(val).__name__)))
+
+    return True
