8363095a2637ba4f9d214b5ac3e1d6f4404e43fd
Ahmet Bakan <lordnapi@gmail.com>
2012-06-04 14:18:54 -0400

    Updated docstring.

--- prody/measure/__init__.py
+++ prody/measure/__init__.py
@@ -25,23 +25,24 @@ Identify contacts
 
 Following class and functions are for contact identifications:
 
-  * :class:`~.Contacts` - identify intermolecular contacts
-  * :func:`~.iterNeighbors` - yield interacting atom pairs
+  * :class:`.Contacts` - identify intermolecular contacts
+  * :func:`.findNeighbors` - identify interacting atom pairs
+  * :func:`.iterNeighbors` - identify interacting atom pairs
 
 Measure quantities
 ==================
 
 Following functions are for measuring simple quantities:
 
-  * :func:`~.calcDistance` - calculate distance(s)
-  * :func:`~.calcAngle` - calculate bond angle
-  * :func:`~.calcDihedral` - calculate dihedral angle
-  * :func:`~.calcOmega` - calculate omega (ω) angle
-  * :func:`~.calcPhi` - calculate phi (φ) angle
-  * :func:`~.calcPsi` - calculate psi (ψ) angle
-  * :func:`~.calcGyradius` - calculate radius of gyration
-  * :func:`~.calcCenter` - calculate geometric (or mass) center
-  * :func:`~.calcDeformVector` - calculate deformation vector
+  * :func:`.calcDistance` - calculate distance(s)
+  * :func:`.calcAngle` - calculate bond angle
+  * :func:`.calcDihedral` - calculate dihedral angle
+  * :func:`.calcOmega` - calculate omega (ω) angle
+  * :func:`.calcPhi` - calculate phi (φ) angle
+  * :func:`.calcPsi` - calculate psi (ψ) angle
+  * :func:`.calcGyradius` - calculate radius of gyration
+  * :func:`.calcCenter` - calculate geometric (or mass) center
+  * :func:`.calcDeformVector` - calculate deformation vector
 
 
 Anisotropic factors
@@ -50,22 +51,22 @@ Anisotropic factors
 Following functions handle anisotropic displacement parameter (ADP) present
 in some X-ray structures.
 
-  * :func:`~.buildADPMatrix` - build ADP matrix 
-  * :func:`~.calcADPAxes` - calculate ADP axes
-  * :func:`~.calcADPs` - calculate ADPs
+  * :func:`.buildADPMatrix` - build ADP matrix 
+  * :func:`.calcADPAxes` - calculate ADP axes
+  * :func:`.calcADPs` - calculate ADPs
 
 Transformations
 ===============
 
 Following class and functions are for handling coordinate transformations:
       
-  * :class:`~.Transformation` - store transformation matrix
-  * :func:`~.alignCoordsets` - align multiple coordinate sets
-  * :func:`~.applyTransformation` - apply a transformation
-  * :func:`~.calcTransformation` - calculate a transformation
-  * :func:`~.calcRMSD` - calculate root-mean-square distance
-  * :func:`~.superpose` - superpose atoms or coordinate sets
-  * :func:`~.moveAtoms` - move atoms by given offset
+  * :class:`.Transformation` - store transformation matrix
+  * :func:`.alignCoordsets` - align multiple coordinate sets
+  * :func:`.applyTransformation` - apply a transformation
+  * :func:`.calcTransformation` - calculate a transformation
+  * :func:`.calcRMSD` - calculate root-mean-square distance
+  * :func:`.superpose` - superpose atoms or coordinate sets
+  * :func:`.moveAtoms` - move atoms by given offset
 """
 
 import prody
