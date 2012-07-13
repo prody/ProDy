5c5879e3c31d3ff578ba100528eaa33fcd53cb03
Ahmet Bakan <lordnapi@gmail.com>
2012-07-13 16:21:35 -0400

    Incremented version number.

--- doc/conf.py
+++ doc/conf.py
@@ -63,7 +63,7 @@ def getRevisionNumber():
     from subprocess import PIPE, Popen
     pipe = Popen('git log --summary'.split(), stdout=PIPE, stderr=PIPE)
     return str(pipe.stdout.read().count('Author:'))
-version = '1.1'
+version = '1.2-beta'
 # The full version, including alpha/beta/rc tags.
 release =  version
 
