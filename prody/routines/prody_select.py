06a5f2699d58978dedad8ddfa9bf2c897105596b
Ahmet Bakan <lordnapi@gmail.com>
2012-08-15 14:02:11 -0400

    Revised prody_select function.

--- prody/routines/prody_select.py
+++ prody/routines/prody_select.py
@@ -22,22 +22,36 @@ __copyright__ = 'Copyright (C) 2010-2012 Ahmet Bakan'
 
 from actions import *
 
-def prody_select(opt):
+def prody_select(**kwargs):
     """Write selected atoms from a PDB file in PDB format."""
 
-    import prody
-    LOGGER = prody.LOGGER
-    pdb = prody.parsePDB(opt.pdb)
-    prefix = opt.output
+    from prody import LOGGER, parsePDB, writePDB
+    
+    pdb = kwargs.get('pdb', None)
+    if not pdb:
+        raise ValueError('pdb argument must be provided')
+    pdb = parsePDB(pdb)
+        
+    selstr = kwargs.get('selstr')
+    if not selstr:
+        raise ValueError('selstr argument must be provided')
+        
+    pdbselect = pdb.select(selstr)
+    if pdbselect is None:
+        LOGGER.warn('Selection {0:s} did not match any atoms.'
+                    .format(repr(selstr)))
+        return
+    LOGGER.info('Selection {0:s} matched {1:d} atoms.'
+                .format(repr(selstr), len(pdbselect)))
+
+    prefix = kwargs.get('output', '')
     if not prefix:
         prefix = pdb.getTitle() + '_selected'
-    pdbselect = pdb.select(opt.selstr)
-    if pdbselect is None:
-        opt.subparser.error('Selection {0:s} do not match any atoms.'
-                      .format(repr(opt.selstr)))
-    LOGGER.info('{0:d} atoms are selected.'.format(len(pdbselect)))
-    LOGGER.info('Writing ' + prefix + '.pdb')
-    prody.writePDB(prefix + '.pdb', pdbselect)
+
+    ext = '.pdb'
+    outname = prefix + (ext if not ext in prefix else '')
+    LOGGER.info('Writing selection into ' + outname)
+    writePDB(outname, pdbselect)
 
 
 def addCommand(commands):
@@ -59,11 +73,11 @@ Fetch PDB 2bfu and write backbone atoms in a file:
   $ prody select 2bfu "backbone" """)
 
 
-    subparser.add_argument('-o', '--output', dest='output', type=str, 
-        metavar='STR', help="output filanem (default: 'pdb_selected.pdb')")
+    subparser.add_argument('-o', '--output', dest='output', metavar='STR', 
+        type=str, help="output PDB filename (default: 'pdb_selected.pdb')")
         
     subparser.add_argument('pdb', help='PDB identifier or filename')
     subparser.add_argument('selstr', help='atom selection string')
 
-    subparser.set_defaults(func=prody_select)
+    subparser.set_defaults(func=lambda ns: prody_select(**ns.__dict__))
     subparser.set_defaults(subparser=subparser)
