482cd212471ad0e3e3400326d3657a6b4a5aae17
Ahmet Bakan <lordnapi@gmail.com>
2012-08-20 12:09:55 -0400

    `prody select` positional arg order is changed to allow multi PDBs.

--- prody/routines/prody_select.py
+++ prody/routines/prody_select.py
@@ -25,33 +25,42 @@ from actions import *
 def prody_select(**kwargs):
     """Write selected atoms from a PDB file in PDB format."""
 
+    from os.path import isfile
     from prody import LOGGER, parsePDB, writePDB
     
-    pdb = kwargs.get('pdb', None)
-    if not pdb:
+    pdbs = kwargs.get('pdb', None)
+    if not pdbs:
         raise ValueError('pdb argument must be provided')
-    pdb = parsePDB(pdb)
-        
     selstr = kwargs.get('selstr')
     if not selstr:
         raise ValueError('selstr argument must be provided')
-        
-    pdbselect = pdb.select(selstr)
-    if pdbselect is None:
-        LOGGER.warn('Selection {0:s} did not match any atoms.'
-                    .format(repr(selstr)))
-        return
-    LOGGER.info('Selection {0:s} matched {1:d} atoms.'
-                .format(repr(selstr), len(pdbselect)))
 
-    prefix = kwargs.get('output', '')
-    if not prefix:
-        prefix = pdb.getTitle() + '_selected'
+    if (isfile(selstr) or len(selstr) == 4) and len(pdbs) == 1:
+        pdbs, selstr = selstr, pdbs[0]
+        LOGGER.warn('The order of selstr and pdb arguments have switched '
+                    'to support multiple files, old order will be supported '
+                    'until v1.4.')
+        pdbs = [pdbs]
 
-    ext = '.pdb'
-    outname = prefix + (ext if not ext in prefix else '')
-    LOGGER.info('Writing selection into ' + outname)
-    writePDB(outname, pdbselect)
+    for pdb in pdbs:    
+        pdb = parsePDB(pdb)
+            
+        pdbselect = pdb.select(selstr)
+        if pdbselect is None:
+            LOGGER.warn('Selection {0:s} did not match any atoms.'
+                        .format(repr(selstr)))
+            return
+        LOGGER.info('Selection {0:s} matched {1:d} atoms.'
+                    .format(repr(selstr), len(pdbselect)))
+
+        prefix = kwargs.get('output', '')
+        if not prefix:
+            prefix = pdb.getTitle() + '_selected'
+
+        ext = '.pdb'
+        outname = prefix + (ext if not ext in prefix else '')
+        LOGGER.info('Writing selection into ' + outname)
+        writePDB(outname, pdbselect)
 
 
 def addCommand(commands):
@@ -68,16 +77,17 @@ def addCommand(commands):
     subparser.set_defaults(usage_example=
     """This command selects specified atoms and writes them in a PDB file.
 
-Fetch PDB 2bfu and write backbone atoms in a file:
+Fetch PDB files 1p38 and 1r39 and write backbone atoms in a file:
         
-  $ prody select 2bfu "backbone" """)
+  $ prody select "backbone" 1p38 1r39""")
 
 
     subparser.add_argument('-o', '--output', dest='output', metavar='STR', 
         type=str, help="output PDB filename (default: 'pdb_selected.pdb')")
         
-    subparser.add_argument('pdb', help='PDB identifier or filename')
     subparser.add_argument('selstr', help='atom selection string')
+    subparser.add_argument('pdb', help='PDB identifier(s) or filename(s)',
+        nargs='+')
 
     subparser.set_defaults(func=lambda ns: prody_select(**ns.__dict__))
     subparser.set_defaults(subparser=subparser)
