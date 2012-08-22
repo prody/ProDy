91feab224684a088c22e320db5503080eb606cbf
Ahmet Bakan <lordnapi@gmail.com>
2012-08-22 16:57:18 -0400

    Improved command to handle non-protein atom selections automatically.

--- prody/routines/prody_align.py
+++ prody/routines/prody_align.py
@@ -24,11 +24,33 @@ from actions import *
 
 __all__ = ['prody_align']
 
-def prody_align(**kwargs):
+def prody_align(*pdbs, **kwargs):
     """Align models in a PDB file or multiple structures in separate PDB files.
+    By default, protein chains will be matched based on selected atoms and 
+    alignment will be performed based on matching residues.  If non-protein
+    atoms are selected and selected atoms match in multiple structures,
+    they will be used for alignment.
     
-    :arg pdb: list of PDB identifiers or filenames
-    :type pdb: list
+    :arg pdbs: :term:`PDB` identifiers or filenames
+    :type pdbs: list
+    
+    **Atom/model selection:**
+
+    :arg select: atom selection string, default is :term:`calpha`
+    :type select: str
+    
+    :arg model: for NMR files, reference model index, default is 1
+    :type model: int
+    
+    **Chain matching options:**
+    
+    :arg seqid: percent sequence identity, default is 90
+    :type seqid: int
+    
+    :arg overlap: percent sequence overlap, default is 90
+    :type overlap: int
+
+    **Output options:**
     
     :arg prefix: prefix for output file, default is PDB filename itself
     :type prefix: str
@@ -36,30 +58,20 @@ def prody_align(**kwargs):
     :arg suffix: output filename suffix, default is ``'_aligned'``
     :type suffix: str
     
-    :arg select: atom selection string
-    :type select: str
-    
-    :arg model: for NMR files, reference model index, default is 1
-    :type model: int
-    
-    :arg seqid: percent sequence identity, default is 90
-    :type seqid: int
-    
-    :arg overlap: percent sequence overlap, default is 90
-    :type overlap: int"""
-            
-    import prody
-    LOGGER = prody.LOGGER
+    """
 
-    pdbs = kwargs.get('pdb')
-    selstr = kwargs.get('select')
+    from numpy import all            
+    from prody import LOGGER, writePDB, parsePDB
+    from prody import alignCoordsets, printRMSD, matchAlign, superpose
+
+    selstr = kwargs.get('select', 'calpha')
     suffix = kwargs.get('suffix', '_aligned')
     if len(pdbs) == 1:
         pdb = pdbs[0]
         LOGGER.info('Aligning multiple models in: ' + pdb)
         prefix = kwargs.get('prefix')
         model = kwargs.get('model')
-        pdb = prody.parsePDB(pdb)
+        pdb = parsePDB(pdb)
         pdbselect = pdb.select(selstr)
         if pdbselect is None:
             subparser = kwargs.get('subparser')
@@ -71,34 +83,61 @@ def prody_align(**kwargs):
         LOGGER.info('{0:d} atoms will be used for alignment.'
                     .format(len(pdbselect)))
         pdbselect.setACSIndex(model-1)
-        prody.printRMSD(pdbselect, msg='Before alignment ')
-        prody.alignCoordsets(pdbselect)
-        prody.printRMSD(pdbselect, msg='After alignment  ')
+        printRMSD(pdbselect, msg='Before alignment ')
+        alignCoordsets(pdbselect)
+        printRMSD(pdbselect, msg='After alignment  ')
         outfn = (prefix or pdb.getTitle()) + suffix + '.pdb'
         LOGGER.info('Writing file: ' + outfn)
-        prody.writePDB(outfn, pdb)
+        writePDB(outfn, pdb)
     else:
+        pdbs = list(pdbs)
         reffn = pdbs.pop(0)
         seqid = kwargs.get('seqid')
         overlap = kwargs.get('overlap')
         LOGGER.info('Aligning structures onto: ' + reffn)
-        ref = prody.parsePDB(reffn)
+        ref = parsePDB(reffn)
+        
+        ref_sel = ref.select(selstr)
+        if ref_sel:
+            LOGGER.info('Selection {0:s} matched {1:d} atoms.'
+                        .format(repr(selstr), len(ref_sel)))
+        else:
+            raise ValueError('selection {0:s} did not match any atoms'
+                               .format(repr(selstr)))
+        match = True
+        if ref_sel.numAtoms('ca') < 2:
+            match = False
+        
         for arg in pdbs:
             if arg == reffn:
                 continue
             #if '_aligned.pdb' in arg:
             #    continue
-            pdb = prody.parsePDB(arg)
-            result = prody.matchAlign(pdb, ref, seqid=seqid, overlap=overlap, 
-                                      tarsel=selstr, allcsets=True,
-                                      cslabel='Model', csincr=1) 
-            if result:
+            LOGGER.info('Evaluating structure: ' + arg)
+            pdb = parsePDB(arg)
+            if match:   
+                result = matchAlign(pdb, ref, seqid=seqid, overlap=overlap, 
+                                    tarsel=selstr, allcsets=True,
+                                    cslabel='Model', csincr=1) 
+                if result:
+                    outfn = pdb.getTitle() + suffix + '.pdb'
+                    LOGGER.info('Writing file: ' + outfn)
+                    writePDB(outfn, pdb)
+                    continue
+
+            pdb_sel = pdb.select(selstr)
+            LOGGER.info('Selection {0:s} matched {1:d} atoms.'
+                        .format(repr(selstr), len(pdb_sel)))
+            if (len(pdb_sel) == len(ref_sel) and 
+                all(pdb_sel.getNames() == ref_sel.getNames())):
+                printRMSD(ref_sel, pdb_sel, msg='Before alignment ')
+                superpose(pdb_sel, ref_sel)
+                printRMSD(ref_sel, pdb_sel, msg='After alignment  ')
                 outfn = pdb.getTitle() + suffix + '.pdb'
                 LOGGER.info('Writing file: ' + outfn)
-                prody.writePDB(outfn, pdb)
+                writePDB(outfn, pdb)
             else:
-                LOGGER.warn('Failed to align ' + arg)
-   
+                LOGGER.warn('Failed to align structure ' + arg + '.')
                 
 def addCommand(commands):
 
@@ -133,30 +172,41 @@ Align 1r39 and 1zz2 onto 1p38 using residues with number less than 300:
 Align all models of 2k39 onto 1aar using residues 1 to 70 (inclusive):
 
     $ prody align --select "resnum 1 to 70" 1aar 2k39 
+
+Align 1fi7 onto 1hrc using heme atoms:
+
+    $ prody align --select "noh heme and chain A" 1hrc 1fi7 
+
     """
     )
         
-    subparser.add_argument('-p', '--prefix', dest='prefix', type=str, 
-        default='', metavar='STR', 
-        help=('output filename prefix (default: PDB filename)'))
-    subparser.add_argument('-x', '--suffix', dest='suffix', type=str, 
-        default='_aligned', metavar='STR', 
-        help=('output filename suffix (default: %(default)s)'))
-    subparser.add_argument('-s', '--select', dest='select', type=str, 
-        default='calpha', metavar='SELSTR',
-        help='selection string (default: "%(default)s")')
-    subparser.add_argument('-m', '--model', dest='model', type=int, 
+    group = subparser.add_argument_group('atom/model selection')
+    group.add_argument('-s', '--select', dest='select', type=str, 
+        default='calpha', metavar='SEL',
+        help='reference structure atom selection  (default: "%(default)s")')
+    group.add_argument('-m', '--model', dest='model', type=int, 
         default=1, metavar='INT',
         help=('for NMR files, reference model index (default: %(default)s)'))
-    subparser.add_argument('-i', '--seqid', dest='seqid', type=int, 
+    
+    group = subparser.add_argument_group('chain matching options')
+    group.add_argument('-i', '--seqid', dest='seqid', type=int, 
         default=90, metavar='INT',
         help=('percent sequence identity (default: %(default)s)'))
-    subparser.add_argument('-o', '--overlap', dest='overlap', type=int, 
+    group.add_argument('-o', '--overlap', dest='overlap', type=int, 
         default=90, metavar='INT',
         help=('percent sequence overlap (default: %(default)s)'))
+
+    group = subparser.add_argument_group('output options')
+    group.add_argument('-p', '--prefix', dest='prefix', type=str, 
+        default='', metavar='STR', 
+        help=('output filename prefix (default: PDB filename)'))
+    group.add_argument('-x', '--suffix', dest='suffix', type=str, 
+        default='_aligned', metavar='STR', 
+        help=('output filename suffix (default: %(default)s)'))
 
     subparser.add_argument('pdb', nargs='+',
         help='PDB identifier(s) or filename(s)')
             
-    subparser.set_defaults(func=lambda opt: prody_align(**opt.__dict__))
+    subparser.set_defaults(func=lambda opt: prody_align(*opt.pdb, 
+                                                         **opt.__dict__))
     subparser.set_defaults(subparser=subparser)
