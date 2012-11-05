3ff9b330c7889f83790453e753a1ef7cfc4f6d8b
Ahmet Bakan <lordnapi@gmail.com>
2012-11-04 21:07:39 -0500

    Move fetchPfamMSA and made fixes identified using pylint.

--- lib/prody/proteins/sequence.py
+++ lib/prody/proteins/sequence.py
@@ -16,151 +16,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-"""This module defines a function for fetching a Multiple Sequence Alignment
-(MSA) from Pfam"""
+"""This module defines functions and classes for parsing, manipulating, and
+analyzing multiple sequence alignments."""
 
 __author__ = 'Anindita Dutta, Ahmet Bakan'
 __copyright__ = 'Copyright (C) 2010-2012 Anindita Dutta, Ahmet Bakan'
 
-__all__ = ['fetchPfamMSA', 'MSAFile', 'MSA', 'parseMSA',
-           'calcShannonEntropy']
+__all__ = ['MSAFile', 'MSA', 'parseMSA', 'calcShannonEntropy']
 
 FASTA = 'fasta'
 SELEX = 'selex'
 STOCKHOLM = 'stockholm'
 
-DOWNLOAD_FORMATS = set(['seed', 'full', 'ncbi', 'metagenomics'])
-FORMAT_OPTIONS = ({'format': set([FASTA, SELEX, STOCKHOLM]),
-                  'order': set(['tree', 'alphabetical']),
-                  'inserts': set(['lower', 'upper']),
-                  'gaps': set(['mixed', 'dots', 'dashes', 'none'])})
 WSJOIN = ' '.join
 ESJOIN = ''.join
 
 import re
-from os.path import join, isfile, getsize, splitext, split
+from os.path import isfile, splitext, split, getsize
 
-from numpy import zeros, dtype
+from numpy import zeros, dtype, array
 
 from prody import LOGGER
-from prody.utilities import makePath, openURL, gunzip, openFile
-
-
-def fetchPfamMSA(acc, alignment='full', folder='.', compressed=False, 
-                 **kwargs):
-    """Return a path to the downloaded Pfam MSA file.
-        
-    :arg acc: Pfam ID or Accession Code
-    :type acc: str
-    
-    :arg alignment: alignment type, one of ``'full'`` (default), ``'seed'``,
-        ``'ncbi'``, or ``'metagenomics'`` 
-    
-    :arg folder: output folder, default is ``'.'``
-    
-    :arg compressed: gzip the downloaded MSA file, default is **False**
-    
-    *Alignment Options*
-    
-    :arg format: a Pfam supported MSA file format, one of ``'selex'``,  
-        (default), ``'stockholm'`` or ``'fasta'``
-    
-    :arg order: ordering of sequences, ``'tree'`` (default) or 
-        ``'alphabetical'`` 
-    
-    :arg inserts: letter case for inserts, ``'upper'`` (default) or ``'lower'``
-    
-    :arg gaps: gap character, one of ``'dashes'`` (default), ``'dots'``, 
-        ``'mixed'`` or **None** for unaligned
-    
-    *Other Options*
-    
-    :arg timeout: timeout for blocking connection attempt in seconds, default 
-        is 5
-
-    :arg outname: out filename, default is input ``'acc_alignment.format'``"""   
-   
-    getAccUrl = 'http://pfam.sanger.ac.uk/family/acc?id=' + acc
-    handle = openURL(getAccUrl)
-    orig_acc = acc
-    acc = handle.readline().strip()
-    url_flag = False
-    
-    if not re.search('(?<=PF)[0-9]{5}$', acc):
-        raise ValueError('No such family: check Pfam ID or Accession Code')
-        
-    
-    if alignment not in DOWNLOAD_FORMATS:
-        raise ValueError('alignment must be one of full, seed, ncbi or'
-                         ' metagenomics')
-    if alignment == 'ncbi' or alignment == 'metagenomics':
-        url = ('http://pfam.sanger.ac.uk/family/' + acc + '/alignment/' +
-               alignment + '/gzipped')
-        url_flag = True
-        extension = '.sth'
-    else:
-        if not kwargs:
-            url = ('http://pfam.sanger.ac.uk/family/' + acc + '/alignment/' +
-                   alignment + '/gzipped')
-            url_flag = True
-            extension = '.sth'
-        else:
-            align_format = kwargs.get('format', 'selex').lower()
-            
-            if align_format not in FORMAT_OPTIONS['format']:
-                raise ValueError('alignment format must be of type selex'
-                                 ' stockholm or fasta. MSF not supported')
-            
-            if align_format == SELEX:
-                align_format, extension = 'pfam', '.slx'
-            elif align_format == FASTA:
-                extension = '.fasta'
-            else:
-                extension = '.sth'
-            
-            gaps = str(kwargs.get('gaps', 'dashes')).lower()
-            if gaps not in FORMAT_OPTIONS['gaps']:
-                raise ValueError('gaps must be of type mixed, dots, dashes, '
-                                 'or None')
-            
-            inserts = kwargs.get('inserts', 'upper').lower()
-            if(inserts not in FORMAT_OPTIONS['inserts']):
-                raise ValueError('inserts must be of type lower or upper')
-            
-            order = kwargs.get('order', 'tree').lower()
-            if order not in FORMAT_OPTIONS['order']:
-                raise ValueError('order must be of type tree or alphabetical')
-            
-            url = ('http://pfam.sanger.ac.uk/family/' + acc + '/alignment/'
-                   + alignment + '/format?format=' + align_format + 
-                   '&alnType=' + alignment + '&order=' + order[0] +
-                   '&case=' + inserts[0] + '&gaps=' + gaps + '&download=1')
-        
-    
-    response =  openURL(url, timeout=int(kwargs.get('timeout', 5)))
-    outname = kwargs.get('outname', None)
-    if not outname:
-        outname = orig_acc
-    filepath = join(makePath(folder), outname + '_' + alignment + extension)
-    if compressed:
-        filepath = filepath + '.gz'
-        if url_flag:
-            f_out = open(filepath, 'wb')
-        else:
-            f_out = openFile(filepath, 'wb')
-        f_out.write(response.read())
-        f_out.close()
-    else:        
-        if url_flag:
-            gunzip(response.read(), filepath)
-        else:
-            with open(filepath, 'wb') as f_out:
-                f_out.write(response.read())
-                
-    LOGGER.info('Pfam MSA for {0} is written as {1}.'
-                .format(orig_acc, filepath))
-    
-    return filepath
+from prody.database import fetchPfamMSA
+from prody.utilities import openFile
 
 SPLITLABEL = re.compile('/*-*').split 
 
@@ -270,14 +148,17 @@ class MSAFile(object):
         """Yield sequences from a Biopython MSA object."""            
         
         aligned = self._aligned
+        lenseq = self._lenseq
         slice = self._slice
         numseq = 0
         for record in self._bio:
             label, seq = record.id, str(record.seq)
+            if not lenseq:
+                self._lenseq = lenseq = len(seq)
             if aligned and lenseq != len(seq):
                 raise IOError('sequence for {0:s} does not have '
                               'expected length {1:d}'
-                              .format(tpl[0], lenseq))
+                              .format(label, lenseq))
             if slice:
                 seq = seq[slice] 
             numseq += 1
@@ -304,7 +185,7 @@ class MSAFile(object):
                     if aligned and lenseq != len(seq):
                         raise IOError('sequence for {0:s} does not have '
                                       'expected length {1:d}'
-                                      .format(tpl[0], lenseq))
+                                      .format(label, lenseq))
                     if slice:
                         seq = seq[slice] 
                     numseq += 1
@@ -340,7 +221,7 @@ class MSAFile(object):
                 if aligned and lenseq != len(seq):
                     raise IOError('sequence for {0:s} does not have '
                                   'expected length {1:d}'
-                                  .format(tpl[0], lenseq))
+                                  .format(label, lenseq))
                 if slice:
                     seq = seq[slice] 
                 numseq += 1
@@ -374,7 +255,7 @@ class MSAFile(object):
             self._filter = None
             return
         
-        if not iscallable(filter):
+        if not callable(filter):
             raise TypeError('filter must be callable')
         
         try: 
@@ -553,7 +434,7 @@ class MSA(object):
         if full:
             return self._labels[index]
         else:
-            return splitLabel(label)[0]
+            return splitLabel(self._labels[index])[0]
                 
     def getResnums(self, index):
         """Return starting and ending residue numbers (:term:`resnum`) for the
@@ -594,7 +475,7 @@ def parseMSA(msa, **kwargs):
         except IOError:
             raise ValueError('msa must be an MSA filename or a Pfam accession')
         else:
-            if compressed in kwargs:
+            if 'compressed' in kwargs:
                 return MSA(filename, **kwargs)
 
     msafile = MSAFile(msa)
@@ -655,24 +536,4 @@ def calcShannonEntropy(msa, dividend=False):
     return entropy
     
     
-if __name__ == '__main__':
 
-    #filepath = fetchPfamMSA('PF00497',alignment='seed',compressed=False,
-    #                         format='stockholm',gaps='dashes')
-    #print filepath
-    #results = list(iterSequences(filepath))
-    
-    filepath1 = fetchPfamMSA('PF00007',alignment='seed',compressed=True, 
-                             timeout=5)
-    filepath2 = fetchPfamMSA('PF00007',alignment='seed',compressed=True, 
-                             format='selex')
-    filepath3 = fetchPfamMSA('PF00007',alignment='seed',compressed=False, 
-                             format='fasta', outname='mymsa')
-    results_sth = list(MSAFile(filepath1))
-    results_slx = list(MSAFile(filepath2))
-    results_fasta = list(MSAFile(filepath3))
-    from Bio import AlignIO
-    alignment = AlignIO.read(filepath3,'fasta')
-    results_obj = list(MSAFile(alignment))
-    import numpy
-    numpy.testing.assert_equal(results_fasta,results_obj)
