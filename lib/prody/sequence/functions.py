25e54462f19bb9297a6b898c7e19501c243ea60c
Ahmet Bakan <lordnapi@gmail.com>
2012-12-09 21:17:38 -0500

    Revised `parseHeatmap`.

--- lib/prody/sequence/functions.py
+++ lib/prody/sequence/functions.py
@@ -16,38 +16,42 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-"""This module defines MSA analysis functions."""
+"""This module defines functions for supporting VMD Heatmapper_ plugin format. 
+
+. _Heatmapper: http://www.ks.uiuc.edu/Research/vmd/plugins/heatmapper/"""
 
 __author__ = 'Ahmet Bakan, Anindita Dutta'
 __copyright__ = 'Copyright (C) 2010-2012 Ahmet Bakan'
 
-from numpy import arange
+from numpy import arange, fromstring, array
 
 from .analysis import *
-from prody import LOGGER
+
 from prody.utilities import openFile
-import numpy as np
+
+from prody import LOGGER
+
 
 __all__ = ['parseHeatmap', 'writeHeatmap', 'showHeatmap']
 
-HM_HEADERLENGTH = 8
-hmheaders = {}
-for title, val in [
-    ('-min', None),
-    ('-xlabel', ''),
-    ('-xorigin', '1'),   
-    ('-xstep', '1'),
-    ('-ylabel', ''),
-    ('-max', None),
-    ('-title', 'Heatmap'),
-    ('-numbering', '')]:
-    hmheaders[title] = val
+HMTYPES = {
+    'max': float,
+    'min': float,
+    'numbering': lambda line: line.split(':'),
+    'title': str,
+    'xlabel': str,
+    'ylabel': str,
+    'xorigin': float,
+    'xstep': float,
+}
+
+
 
 def showHeatmap(heatmap, clim=None, *args, **kwargs):
-    """This shows a heatmap plot. Input can be an 2D matrix
-    or an outfile file from VMD's "Heatmapper" plugin. 
+    """Show *heatmap*, which can be an 2D matrix or an :file:`.hm` file in 
+    VMD Heatmapper plugin  format. 
     
-    HeatMap is plotted using :func:`~matplotlib.pyplot.imshow`
+    Heatmap is plotted using :func:`~matplotlib.pyplot.imshow`
     function."""
 
     try:
@@ -59,9 +63,10 @@ def showHeatmap(heatmap, clim=None, *args, **kwargs):
     if ndim != 2:
         raise ValueError('mutinfo must be a 2D matrix')
     
-    if not kwargs.has_key('interpolation'):
+    if not 'interpolation' in kwargs:
         kwargs['interpolation'] = 'nearest'
-    if not kwargs.has_key('origin'):
+    
+    if not 'origin' in kwargs:
         kwargs['origin'] = 'lower'
     
     y, x = shape
@@ -106,48 +111,61 @@ def showHeatmap(heatmap, clim=None, *args, **kwargs):
     return show
 
 
-def parseHeatmap(hmfile, **kwargs):
-    """This parses a HeatMap outfile file from VMD's "Heatmapper" plugin.
-    Input is ``".hm"`` file. Returns a tuple containing a dictionary of
-    headers and a numpy array containing the vales plotted in Heatmapper."""    
+def parseHeatmap(filename, **kwargs):
+    """Return a two dimensional array and a dictionary with information parsed
+    from :file:`.hm` file in VMD Heatmapper plugin format."""    
     
-    from os.path import isfile
+    meta = {}
+    arrs = []
+    with openFile(filename) as hm:
+        for line in hm:
+            if line.startswith('-'):
+                label, data = line[1:].split(None, 1)
+                data = data.strip()
+                if data[0] == data[-1] == '"':
+                    data = data[1:-1]
+                label = label.strip()
+                try:
+                    meta[label] = HMTYPES[label](data)
+                except KeyError: 
+                    LOGGER.warn('Unrecognized label encountered: {0}'
+                                .format(repr(label)))
+                    meta[label] = HMTYPES[label](data)
+                except TypeError:
+                    LOGGER.warn('Could not parse data with label {0}.'
+                                .format(repr(label)))
+            else:
+                arrs.append(line.rstrip())
+    nnums = len(meta.get('numbering', '')) 
+    heatmap = []
+    numbers = []
 
-    if isfile(str(hmfile)):
-        with openFile(hmfile) as file:
-            head = [file.next() for x in xrange(HM_HEADERLENGTH)]
-    else:
-        raise ValueError('Input file not a valid file')
-    
-    headers = {}
-    for i in xrange(8):
-        temp = head[i].split()
-        if len(temp) < 2:
-            raise TypeError('Cannot parse header of hm, check file format')
-        headers[temp[0]] = ' '.join(temp[1:])
-               
-    if set.difference(set(headers.keys()), set(hmheaders.keys())):
-        raise TypeError('Cannot parse header of hm, check file format')
-    
-    try:
-        hmArray = np.loadtxt(hmfile, delimiter=';',
-                             skiprows=HM_HEADERLENGTH, dtype='|S')
-    except:
-        raise TypeError('Cannot parse heatmap file {0:s}, check file format'
-                        .format(hmfile))
-    if np.all(hmArray[:,-1] == ' '):
-        hmArray = np.delete(hmArray,hmArray.shape[1]-1, -1)
-    y, x = hmArray.shape
-    
-    for i in xrange(y):
-        temp1, temp2, firstelem = hmArray[i,0].split(':')
-        hmArray[i,0] = firstelem
-    hmArray = hmArray.astype(np.float)
+    for arr in arrs:
+        if nnums:
+            items = arr.split(':', nnums + 1)
+            numbers.append(items[:nnums])
+        else:
+            items = [arr]
+        heatmap.append(fromstring(items[-1], float, sep=';'))
         
-    return headers, hmArray
+    heatmap = array(heatmap)
+    if nnums:
+        numbering = meta['numbering']
+        try:
+            numbers = array(numbers, int)
+        except ValueError:
+            try:
+                numbers = array(numbers, float)
+            except ValueError:            
+                LOGGER.warn('Numbering for y-axis could not be parsed.')
+                numbering = []
+        for i, label in enumerate(numbering):
+            meta[label] = numbers[:, i].copy()
+        
+    return heatmap, meta
             
         
-def writeHeatmap(heatmap, prefix=None, **kwargs):
+def writeHeatmap(filename, heatmap, prefix=None, **kwargs):
     """This writes the input 2D array into a ``"".hm""`` file in the same
     format as VMD's Heapmapper plugin. Input arguments may contain the files`
     header information like ``"-max"``, ``"-min""`` , ``"xorigin"``,
@@ -161,10 +179,9 @@ def writeHeatmap(heatmap, prefix=None, **kwargs):
     if ndim!= 2:
         raise TypeError('heatmap should be a 2D array')
     
-    if not prefix:
-        prefix = 'HeatMapFile'
-    outname = prefix + '.hm'
-    file = openFile(outname, 'wb')
+    if not filename.lower().endswith('.hm'):
+        filename += '.hm'
+    out = openFile(filename, 'wb')
     
     for key in hmheaders:
         item = kwargs.get(key[1:], hmheaders[key])
@@ -175,22 +192,22 @@ def writeHeatmap(heatmap, prefix=None, **kwargs):
             if item is None:
                 item = '{0:.2f}'.format(heatmap.max())
         line = key + ' "' + str(item) + '"\n'
-        file.write(line)
+        out.write(line)
     
     col1 = kwargs.get('indices', None)
     if col1 == None or len(col1) != shape[0]:
-        col1 = np.arange(1, shape[0]+1)
-    col2 = np.arange(0, shape[0])
+        col1 = arange(1, shape[0]+1)
+    col2 = arange(0, shape[0])
     
     firstelem = map(lambda x,y,z: x+':'+y+':'+z, map(str, col1),
                     map(str, col2), map(str, heatmap[:,0]))
-    heatmap = np.array(map(str, heatmap.reshape(heatmap.size))).reshape(
+    heatmap = array(map(str, heatmap.reshape(heatmap.size))).reshape(
         (shape[0], shape[1]))
     heatmap[:,0] = firstelem
     for i in xrange(shape[0]):
-        file.write((';'.join(heatmap[i,:])+'\n'))
-    file.close()
+        out.write((';'.join(heatmap[i,:])+'\n'))
+    out.close()
     LOGGER.info('Heatmap file is written as {0:s}'.format(outname))
         
-        
+    return filename
     
