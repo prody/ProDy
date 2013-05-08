7cc7493cf17f475871a4aeaa1f57807c26466dab
Ahmet Bakan <lordnapi@gmail.com>
2013-05-08 15:40:11 -0400

    Updated configuration files.

--- doc/tutorials/conf.py
+++ doc/tutorials/conf.py
@@ -2,7 +2,7 @@ try:
     execfile('../../conf.py')
 except NameError:
     exec(open('../../conf.py').read())
-    
+
 if not os.path.isfile('../prody-objects.inv'):
 
     def trim():
@@ -12,14 +12,21 @@ if not os.path.isfile('../prody-objects.inv'):
             raise ValueError
         import zlib
         trimmed = []
-        
+
         for line in zlib.decompress(inp.read()).decode('utf-8').splitlines():
             items = line.split(' py:')
-            
+
             if len(items) > 1:
-                if (items[1].startswith('method') or 
+                if (items[1].startswith('method') or
                     items[1].startswith('attribute')):
                     items[0] = '.'.join(items[0].split('.')[-2:])
+                elif items[1].startswith('module'):
+                    _ = items[0].split('.')
+                    if len(_) > 1:
+                        if _[-1] == _[-2]:
+                            items[0] = '.'.join(_[-2:] or _)
+                        else:
+                            items[0] = _[-1]
                 else:
                     items[0] = items[0].split('.')[-1]
                 trimmed.append(items[0] + ' py:' + items[1])
@@ -40,22 +47,33 @@ if not os.path.isfile('../prody-objects.inv'):
 
     trim()
 
-intersphinx_mapping['prody'] = ('http://csb.pitt.edu/ProDy', 
+    def get_inv():
+        from sphinx.ext.intersphinx import read_inventory_v2
+        f = open('../prody-objects.inv')
+        f.readline()
+        return read_inventory_v2(f, '.', os.path.join)
+
+
+intersphinx_mapping['prody'] = ('http://csb.pitt.edu/ProDy',
 								'../prody-objects.inv')
 
 master_doc = 'index'
 
 version = release = tutorial_version or version
 latex_documents = [
-  ('index', 
-   os.path.basename(os.getcwd()) + '.tex', 
+  ('index',
+   os.path.basename(os.getcwd()) + '.tex',
    tutorial_title,
-   tutorial_author, 
+   tutorial_author,
    'manual'),
 ]
 latex_logo = tutorial_logo or '_static/logo.png'
 latex_show_urls = 'footnote'
 
 html_additional_pages = {}
+html_domain_indices = False
+html_use_index = False
+
+latex_domain_indices = True
 
 plot_working_directory = os.path.join(os.getcwd(), '..', '..', '_doctest')
