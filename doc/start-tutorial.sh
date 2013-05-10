3e6fa6addb7aa04252d251c287f7d93e32eed985
Ahmet Bakan <lordnapi@gmail.com>
2013-05-10 16:30:11 -0400

    Making a link to _templates folder for HTML building.

--- doc/start-tutorial.sh
+++ doc/start-tutorial.sh
@@ -1,17 +1,17 @@
 
 read -p "Enter tutorial title (e.g. ENM Analysis using ProDy): " TITLE
 while [ -z "$TITLE" ]
-do 
+do
 	read -p "Enter tutorial title: " TITLE
 done
 read -p "Enter a short title (e.g. ENM Analysis): " SHORT
 while [ -z "$SHORT" ]
-do 
+do
 	read -p "Enter a short title: " SHORT
 done
 read -p "Enter author name (seperate multiple names with comma): " AUTHOR
 while [ -z "$AUTHOR" ]
-do 
+do
 	read -p "Enter author name: " AUTHOR
 done
 SHORT=${SHORT,,}
@@ -22,10 +22,11 @@ REFERENCE=${SHORT// /-}
 mkdir -p tutorials/$TUTORIAL
 mkdir -p tutorials/$TUTORIAL/$TUTORIAL\_files
 ln -fs ../../_static tutorials/$TUTORIAL
+ln -fs ../../_templates tutorials/$TUTORIAL
 cp -f tutorials/template/Makefile tutorials/$TUTORIAL
 sed 's/AUTHOR/'"$AUTHOR"'/g' tutorials/template/conf.py > tutorials/$TUTORIAL/conf.py
 sed -i 's/TITLE/'"$TITLE"'/g' tutorials/$TUTORIAL/conf.py
-sed 's/TITLE/'"$TITLE"'/g' tutorials/template/index.rst > tutorials/$TUTORIAL/index.rst 
+sed 's/TITLE/'"$TITLE"'/g' tutorials/template/index.rst > tutorials/$TUTORIAL/index.rst
 sed -i 's/REFERENCE/'"$REFERENCE"'/g' tutorials/$TUTORIAL/index.rst
 sed 's/TUTORIAL/'"$TUTORIAL"'/g' tutorials/template/intro.rst > tutorials/$TUTORIAL/intro.rst
 echo
