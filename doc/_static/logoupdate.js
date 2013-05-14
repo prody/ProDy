23a25414547746d91cc61aeb0ab58e6f68aa127d
Ahmet Bakan <lordnapi@gmail.com>
2013-05-14 09:38:52 -0400

    Revised javascript files.

--- doc/_static/logoupdate.js
+++ doc/_static/logoupdate.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
 
     function updateLogo() {
       if (document.URL.search("\/plugins\/") > -1 || document.URL.search("panel1-4") > -1) {
-        document.getElementById("logo").src = "/_static/nmwiz.png";
+        document.getElementById("logo").src = "http://www.csb.pitt.edu/ProDy/_static/nmwiz.png";
       } else if (document.URL.search("evol") > -1 || document.URL.search("panel1-3") > -1) {
-        document.getElementById("logo").src = "/_static/evol.png";
+        document.getElementById("logo").src = "http://www.csb.pitt.edu/ProDy/_static/evol.png";
       }
 
     }
--- doc/_static/textresize.js
+++ doc/_static/textresize.js
@@ -1,51 +1,34 @@
 $(document).ready(function() {
 
-  fsUnit = 'px';
-  defaultFS = '12';
+  $.cookie.settings = {
+      path : "/",
+      domain : "",
+      expires : 30
+  };
+
+  var unit = "pt";
+  var size = parseInt($.cookie("ProDyDocFontSize"));
+  var defsize = 9;
+
+  if (size) {
+      $("body").css("font-size", size + unit);
+  } else {
+      size = defsize
+  }
+
   function changeFontSize(incr) {
-    if (document.body.style.fontSize == "") {
-      document.body.style.fontSize = defaultFS + fsUnit;
-    }
-    if (incr == 0) {
-      document.body.style.fontSize = defaultFS + fsUnit;
+    size = size;
+    if (incr) {
+      size += incr;
     } else {
-      document.body.style.fontSize = (parseFloat(document.body.style.fontSize) + incr ) + fsUnit;
+      size = defsize;
     }
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + 365);
-    document.cookie= "ProDyDocFontSize" + "=" + escape(parseFloat(document.body.style.fontSize)) + ";expires=" + exdate.toUTCString();
+    $("body").css("font-size", size + unit);
+    $.cookie("ProDyDocFontSize", size);
   }
 
-  function setFontSize(fs) {
-    document.body.style.fontSize = fs + fsu;
-    var exdate = new Date();
-    exdate.setDate(exdate.getDate() + 365);
-    document.cookie= "ProDyDocFontSize" + "=" + escape(fs) + ";expires=" + exdate.toUTCString();
-  }
-
-  function getFontSize() {
-    if (document.cookie.length > 0) {
-      c_name = "ProDyDocFontSize"
-      var c_start = document.cookie.indexOf(c_name + "=");
-      if (c_start != -1) {
-        c_start = c_start + c_name.length + 1;
-        var c_end = document.cookie.indexOf(";", c_start);
-        if (c_end == -1) c_end = document.cookie.length;
-          return unescape(document.cookie.substring(c_start, c_end));
-      }
-    }
-    return "";
-  }
-
-  function checkFontSize() {
-    var fs = getFontSize('SphinxFontSize');
-    if (fs != null && fs != "") {
-      document.body.style.fontSize = fs + fsUnit;
-    }
-  }
   $('#text-larger').click(function() {changeFontSize(1)});
   $('#text-smaller').click(function() {changeFontSize(-1)});
   $('#text-reset').click(function() {changeFontSize(0)});
-  checkFontSize();
 
 });
