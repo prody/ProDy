b7407a21bf7419a8af8ce6a6a4b5dcf4e031c257
Ahmet Bakan <lordnapi@gmail.com>
2013-05-20 11:00:27 -0400

    Revised logo updater.

--- doc/_static/js/logoupdate.js
+++ doc/_static/js/logoupdate.js
@@ -1,12 +1,8 @@
 $(document).ready(function() {
 
-    function updateLogo() {
-      if (document.URL.search("\/plugins\/") > -1 || document.URL.search("panel1-4") > -1) {
-        document.getElementById("logo").src = "http://www.csb.pitt.edu/ProDy/_static/nmwiz.png";
-      } else if (document.URL.search("evol") > -1 || document.URL.search("panel1-3") > -1) {
-        document.getElementById("logo").src = "http://www.csb.pitt.edu/ProDy/_static/evol.png";
-      }
-
-    }
-    updateLogo();
+  if (document.URL.search("plugins") > -1 || document.URL.search("nmwiz") > -1) {
+    document.getElementById("logo").src = "http://www.csb.pitt.edu/ProDy/_static/nmwiz.png";
+  } else if (document.URL.search("evol") > -1 || document.URL.search("panel1-3") > -1) {
+    document.getElementById("logo").src = "http://www.csb.pitt.edu/ProDy/_static/evol.png";
+  }
 });
