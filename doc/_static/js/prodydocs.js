735f2337ee663dde1ae95bb3d000efbc8b79b49a
Ahmet Bakan <lordnapi@gmail.com>
2013-05-28 16:48:06 -0400

    Updated documentation files.

--- doc/_static/js/prodydocs.js
+++ doc/_static/js/prodydocs.js
@@ -5,14 +5,26 @@ $(document).ready(function() {
   var items = $("#localtoc a")
   if (items.length) {
     items.prepend('<i class="icon-chevron-right pull-right"></i>')
-    items[0].href = "#" + $(".headerlink")[0].href.split("#")[1]
+
+    var sections = $(".section");
+    var section_id = sections[0].id;
+    if (section_id.indexOf('.') >= 0) {
+      items[0].href = "#" + $(sections[0]).find('>:first-child').attr('id');
+    } else {
+      items[0].href = "#" + section_id;
+    }
+
+    items.first().addClass("navfirst")
+    items.last().addClass("navlast")
     $("#localtoc ul").addClass("nav nav-list")
-    $("#localtoc > ul").addClass("bs-docs-sidenav")
+    $("#localtoc > ul").addClass("toc")
   }
+
+  //$('#localtoc').scrollspy({'offset': 190})
 
 
   setTimeout(function () {
-      $('.bs-docs-sidenav,.subnav').affix({
+      $('.toc,.subnav').affix({
         offset: {
           top: 140,
           bottom: 20
