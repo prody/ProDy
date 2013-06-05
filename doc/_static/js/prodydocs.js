664aad4ff94dfcae0f643e245d2da1ab9480612e
Ahmet Bakan <lordnapi@gmail.com>
2013-06-05 13:38:05 -0400

    Updated front page.

--- doc/_static/js/prodydocs.js
+++ doc/_static/js/prodydocs.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
 
   var $window = $(window)
@@ -31,26 +30,42 @@ $(document).ready(function() {
 
   $('.collapse').not('#collapseToolbox').collapse('hide');
 
-  $('#carousel').carousel({interval: 100000}).on({
-      slid: function(){
-        var pills = $('.nav-pills > li');
-        pills.removeClass('active');
-        var index = $('#carousel .item.active').index('#carousel .item');
-        $(pills[index]).addClass('active');
-        if (index == 1) {
-          document.getElementById("logo").src = "_static/evol.png";
-        } else if (index == 2) {
-          document.getElementById("logo").src = "_static/nmwiz.png";
-        } else {
-          document.getElementById("logo").src = "_static/logo.png";
-        }
-
-      }});
-
-  if (document.URL.search("plugins") > -1 || document.URL.search("nmwiz") > -1) {
-    document.getElementById("logo").src = "http://www.csb.pitt.edu/ProDy/_static/nmwiz.png";
-  } else if (document.URL.search("evol") > -1 || document.URL.search("panel1-3") > -1) {
-    document.getElementById("logo").src = "http://www.csb.pitt.edu/ProDy/_static/evol.png";
+  // Alter logo based on URL
+  var url = document.URL;
+  var logo = document.getElementById("logo");
+  if (url.search("plugins") > -1 || url.search("nmwiz") > -1) {
+    logo.src = "http://www.csb.pitt.edu/ProDy/_static/nmwiz.png";
+  } else if (url.search("evol") > -1 || url.search("panel1-3") > -1) {
+    logo.src = "http://www.csb.pitt.edu/ProDy/_static/evol.png";
   }
 
+  var carousel = $('#carousel');
+  if (carousel.length) {
+    carousel.carousel({'interval': false});
+    carousel.on({
+        slid: function(){
+          var pills = $('.nav-pills > li');
+          pills.removeClass('active');
+          var index = $('#carousel .item.active').index('#carousel .item');
+          $(pills[index]).addClass('active');
+          if (index == 1) {
+            document.getElementById("logo").src = "_static/evol.png";
+          } else if (index == 2) {
+            document.getElementById("logo").src = "_static/nmwiz.png";
+          } else {
+            document.getElementById("logo").src = "_static/logo.png";
+          }
+
+        }});
+    // Move carousel based on URL
+    if (url.search('evol') > -1) {
+      carousel.carousel(1);
+    } else if (url.search('nmwiz') > -1) {
+      carousel.carousel(2);
+    } else if (url.search('downloads') > -1) {
+      carousel.carousel(3);
+    } else if (url.search('tutorials') > -1) {
+      carousel.carousel(4);
+    }
+  }
 });
