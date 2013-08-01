d7866574ae843373fb65d89a63cc95987aef778a
Ahmet Bakan <lordnapi@gmail.com>
2013-08-01 09:53:31 -0400

    Changed matplotlib backend so plotting tests don't fail due not finding a display.

--- lib/prody/tests/__init__.py
+++ lib/prody/tests/__init__.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # ProDy: A Python Package for Protein Dynamics Analysis
-# 
+#
 # Copyright (C) 2010-2012 Ahmet Bakan
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#  
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
@@ -22,7 +22,7 @@
   from prody import *
   prody.test()
 
-Testing will use :mod:`nose` if it is available, otherwise it will use 
+Testing will use :mod:`nose` if it is available, otherwise it will use
 :mod:`unittest`."""
 
 __author__ = 'Ahmet Bakan'
@@ -49,6 +49,7 @@ except ImportError:
     MATPLOTLIB = False
 else:
     MATPLOTLIB = True
+    matplotlib.user('Agg')
 
 TESTDIR = abspath(split(inspect.getfile(inspect.currentframe()))[0])
 TEMPDIR = tempfile.gettempdir()
@@ -58,14 +59,14 @@ PREFIX = 'prody.tests.'
 
 for pyfile in glob(join(TESTDIR, '*', '*.py')):
     pyfile = splitext(relpath(pyfile, TESTDIR))[0]
-    
+
     items = pyfile.split(dirsep)
-    if items[-1] == '__init__': 
+    if items[-1] == '__init__':
         items = items[:-1]
 
     if items[-1].startswith('test_'):
         MODULES['.'.join([i[5:] for i in items])] = PREFIX + '.'.join(items)
-    
+
 
 def runTests(*mods, **kwargs):
 
@@ -76,25 +77,25 @@ def runTests(*mods, **kwargs):
                 modules.append(MODULES[mod])
             except KeyError:
                 raise ValueError(mod + ' is not a valid test module name')
-    else: 
+    else:
         modules = MODULES.values() # PY3K: OK
 
     try:
         import nose
-        
+
     except ImportError:
 
         LOGGER.warning('Failed to import nose, using unittest for testing.')
         LOGGER.info('nose is available at http://readthedocs.org/docs/nose/')
         import unittest
         from sys import stderr
-        
+
         verbosity = kwargs.get('verbose', 2)
         descriptions = kwargs.get('descriptions', True)
         stream = kwargs.get('stream', stderr)
-        
+
         testrunner = unittest.TextTestRunner(stream, descriptions, verbosity)
-            
+
         for module in modules:
             testrunner.run(unittest.defaultTestLoader.
                            loadTestsFromName(module))
@@ -108,5 +109,6 @@ def runTests(*mods, **kwargs):
                 Tester(module).test(label=label, verbose=verbose)
         else:
             Tester('prody.tests').test(label=label, verbose=verbose)
+
 if __name__ == '__main__':
     runTests()
