9ba8a0614f8b3edf1cf00df02161669c8623196c
Ahmet Bakan <lordnapi@gmail.com>
2013-11-13 15:24:21 -0500

    Updated referenec doc config for RTFD.

--- doc/reference/conf.py
+++ doc/reference/conf.py
@@ -1,7 +1,10 @@
-try:
-    execfile('../conf.py')
-except NameError:
-    exec(open('../conf.py').read())
+exec(open('../conf.py').read())
+
+extensions = extensions[:10]
+extensions.extend([
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive'
+    ])
 
 sys.path[-2] = os.path.abspath('../_sphinxext')
 sys.path[-1] = os.path.abspath('../_inventory')
