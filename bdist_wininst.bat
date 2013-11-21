7da44addb6f86edb66c2e1c041dcf27915c6accd
Ahmet Bakan <lordnapi@gmail.com>
2013-11-21 15:31:16 -0500

    Added bdist preparation file, removed devel script [skip ci].

---
+++ bdist_wininst.bat
@@ -0,0 +1,5 @@
+del /S /Q /F build\*.*
+C:\Python26\python.exe setup.py bdist_wininst
+C:\Python27\python.exe setup.py bdist_wininst
+C:\Python32\python.exe setup.py bdist_wininst
+C:\Python33\python.exe setup.py bdist_wininst
--- devel_remove_contrib.sh
+++
@@ -1,4 +0,0 @@
-rm -f lib/prody/kdtree/*c
-rm -f lib/prody/atomic/pyparsing*py
-rm -f lib/prody/apps/argparse.py
-rm -f lib/prody/proteins/*pairwise2.*
