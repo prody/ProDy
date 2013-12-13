d73f86800d08b04cb8bafb6803921ec8042b3f45
Ahmet Bakan <lordnapi@gmail.com>
2013-12-13 18:07:44 -0500

    Removed ProDy-website related JS code [skip ci].

--- docs/_static/js/prodydocs.js
+++ docs/_static/js/prodydocs.js
@@ -39,50 +39,18 @@ $(document).ready(function() {
     logo.src = "http://www.csb.pitt.edu/ProDy/_static/evol.png";
   }
 
-  var carousel = $('#carousel');
-  if (carousel.length) {
-    carousel.carousel({'interval': false});
-    carousel.on({
-        slid: function(){
-          var pills = $('.nav-pills > li');
-          pills.removeClass('active');
-          var index = $('#carousel .item.active').index('#carousel .item');
-          var pill = $(pills[index]);
-          $(pill).addClass('active');
-          if (index == 1) {
-            document.getElementById("logo").src = "_static/evol.png";
-          } else if (index == 2) {
-            document.getElementById("logo").src = "_static/nmwiz.png";
-          } else {
-            document.getElementById("logo").src = "_static/logo.png";
-          }
-          window.location.hash = pill.find('a').attr('href').slice(1);
-        }});
-    // Move carousel based on URL
-    if (url.search('evol') > -1 || url.search('panel1-3') > -1) {
-      carousel.carousel(1);
-    } else if (url.search('nmwiz') > -1) {
-      carousel.carousel(2);
-    } else if (url.search('downloads') > -1) {
-      carousel.carousel(3);
-    } else if (url.search('tutorials') > -1) {
-      carousel.carousel(4);
+  // Downloads
+  $('tt.download').append('&nbsp;<i class="icon-download"></i>')
+  var ext = $('a.external');
+  ext.attr('target', '_blank');
+  ext.each(function(i) {
+    var jthis = $(this);
+    var pre = jthis.find('span.pre');
+    if (pre.length) {
+      pre.append('&nbsp;<i class="icon-share"></i>');
+    } else {
+      jthis.append('&nbsp;<i class="icon-share"></i>');
     }
-  } else {
-    // Downloads
-    $('tt.download').append('&nbsp;<i class="icon-download"></i>')
-    var ext = $('a.external');
-    ext.attr('target', '_blank');
-    ext.each(function(i) {
-      var jthis = $(this);
-      var pre = jthis.find('span.pre');
-      if (pre.length) {
-        pre.append('&nbsp;<i class="icon-share"></i>');
-      } else {
-        jthis.append('&nbsp;<i class="icon-share"></i>');
-      }
-    });
-
-  }
+  });
 
 });
