ANM mode calculation, MSA new merge method, bug fixes on psicov

prody/dynamics/anm.py
@@ -215,7 +215,7 @@ class ANMBase(NMA):
             if eigvals:
                 turbo = False
             if isinstance(self._hessian, np.ndarray):
-                vectors, values, dummy = linalg.svd(self._hessian)
+                values, vectors = linalg.eigh(self._hessian)
             else:
                 try:
                     from scipy.sparse import linalg as scipy_sparse_la
@@ -236,8 +236,8 @@ class ANMBase(NMA):
         else:
             if n_modes is not None:
                 LOGGER.info('Scipy is not found, all modes are calculated.')
-            vectors, values, dummy = linalg.svd(self._hessian)
-        n_zeros = sum(values < ZERO)
+            values, vectors = linalg.eigh(self._hessian)
+        n_zeros = sum(values < ZERO) 
         if n_zeros < 6:
             LOGGER.warning('Less than 6 zero eigenvalues are calculated.')
             shift = n_zeros - 1