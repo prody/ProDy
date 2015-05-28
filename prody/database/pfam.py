df075b482041690a7ecfeda38ebce3631b69b60c
cihankayacihan <cihankayacihan@gmail.com>
2015-05-28 16:17:07 -0400

    small bug fixes and addition of release files

--- prody/database/pfam.py
+++ prody/database/pfam.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """This module defines functions for interfacing Pfam database."""
 
-__author__ = 'Anindita Dutta, Ahmet Bakan'
+__author__ = 'Anindita Dutta, Ahmet Bakan, Cihan Kaya'
 
 import re
 import urllib, urllib2
