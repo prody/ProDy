eigenvalue calculation fix

prody/dynamics/anm.py
@@ -217,7 +217,9 @@ class ANMBase(NMA):
             if isinstance(self._hessian, np.ndarray):
                 values, vectors = linalg.eigh(self._hessian, turbo=turbo,
                                               eigvals=eigvals)
-                n_zeros = sum(values < ZERO)  
+            n_zeros = sum(values < ZERO)
+            if n_zeros > n_modes:
+                   values, vectors = linalg.eigh(self._hessian, turbo=turbo)
 
             else:
                 try:
@@ -240,7 +242,8 @@ class ANMBase(NMA):
             if n_modes is not None:
                 LOGGER.info('Scipy is not found, all modes are calculated.')
             values, vectors = linalg.eigh(self._hessian)
-        n_zeros = sum(values < ZERO) 
+        n_zeros = sum(values < ZERO)
+
         if n_zeros < 6:
             LOGGER.warning('Less than 6 zero eigenvalues are calculated.')
             shift = n_zeros - 1
@@ -249,7 +252,10 @@ class ANMBase(NMA):
             shift = n_zeros - 1
         if zeros:
             shift = -1
-        self._eigvals = values[1+shift:]
+        if n_zeros > n_modes:
+            self._eigvals = values[1+shift:1+shift+n_modes]
+        else:
+            self._eigvals = values[1+shift:]
         self._vars = 1 / self._eigvals
         self._trace = self._vars.sum()
         if shift: