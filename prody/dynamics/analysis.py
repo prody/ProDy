8e97d2f8f9efd8c87655d90c243929fa61b0bc1c
cihankayacihan <cihankayacihan@gmail.com>
2017-12-18 12:54:44 -0500

    changes for net entropy transfer

--- prody/dynamics/analysis.py
+++ prody/dynamics/analysis.py
@@ -24,7 +24,8 @@ __all__ = ['calcCollectivity', 'calcCovariance', 'calcCrossCorr',
            'calcProjection', 'calcCrossProjection', 
            'calcPerturbResponse', 'parsePerturbResponseMatrix', 
            'calcPerturbResponseProfiles', 'writePerturbResponsePDB',
-           'calcSpecDimension', 'calcPairDeformationDist',]
+           'calcSpecDimension', 'calcPairDeformationDist',
+           'calcEntropyTransfer', 'calcOverallNetEntropyTransfer']
 
 class PRSMatrixParseError(Exception):
     pass
@@ -1537,16 +1538,17 @@ def calcEntropyTransfer(model, ind1, ind2, tau):
     n_atoms = model.numAtoms()
     n_modes = model.numModes()
 
-    eigvecs = model.getEigvecs()
+    eigvecs = model.getEigvecs().T
     eigvals = model.getEigvals()
-    
+
+    tau_0 = 1
     T = 0
     dummy1 = 0
     dummy2 = 0
     for k in range(n_modes):
-        dummy1 += eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2]
-        dummy2 += eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2] * exp(-eigvals[k]*tau/tau_0)
-    T += 0.5 * log(dummy1**2 + dummy2**2)
+        dummy1 += 1.0 / eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2]
+        dummy2 += 1.0 / eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2] * np.exp(-eigvals[k]*tau/tau_0)
+    T += 0.5 * np.log(dummy1**2 - dummy2**2)
 
     dummy1 = 0
     dummy2 = 0
@@ -1559,24 +1561,24 @@ def calcEntropyTransfer(model, ind1, ind2, tau):
     dummy9 = 0 
     dummy10 = 0 
     for k in range(n_modes):
-        dummy1 += eigvals[k] * eigvecs[k,ind1] * eigvecs[k,ind1]
-        dummy2 += eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2]
-        dummy3 += eigvals[k] * eigvecs[k,ind1] * eigvecs[k,ind2]
-        dummy4 += eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2] * exp(-eigvals[k]*tau/tau_0)
-        dummy5 += eigvals[k] * eigvecs[k,ind1] * eigvecs[k,ind2] * exp(-eigvals[k]*tau/tau_0)  
-        dummy9 += eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2] * exp(-eigvals[k]*tau/tau_0)       
+        dummy1 += 1.0 / eigvals[k] * eigvecs[k,ind1] * eigvecs[k,ind1]
+        dummy2 += 1.0 / eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2]
+        dummy3 += 1.0 / eigvals[k] * eigvecs[k,ind1] * eigvecs[k,ind2]
+        dummy4 += 1.0 / eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2] * np.exp(-eigvals[k]*tau/tau_0)
+        dummy5 += 1.0 / eigvals[k] * eigvecs[k,ind1] * eigvecs[k,ind2] * np.exp(-eigvals[k]*tau/tau_0)  
+        dummy9 += 1.0 / eigvals[k] * eigvecs[k,ind2] * eigvecs[k,ind2] * np.exp(-eigvals[k]*tau/tau_0)       
         
     dummy6 = dummy5
     dummy7 = dummy3 
     dummy8 = dummy2
     dummy10 = dummy1
 
-    T -= 0.5 * log(dummy1*dummy2**2+2*dummy3*dummy4*dummy5-(dummy6**2+dummy7**2)*dummy8-dummy9**2*dummy10)
-    T -= 0.5 * log(dummy2)
-    T += 0.5 * log(dummy1*dummy2-dummy3**2)
+    T -= 0.5 * np.log(dummy1*dummy2**2+2*dummy3*dummy4*dummy5-(dummy6**2+dummy7**2)*dummy8-dummy9**2*dummy10)
+    T -= 0.5 * np.log(dummy2)
+    T += 0.5 * np.log(dummy1*dummy2-dummy3**2)
     return T
 
-def calculateAllEntropyTransfer(model, tau):
+def calcAllEntropyTransfer(model, tau):
     """This function calculates the net entropy transfer for a whole structure 
     with a given time constant tau based on GNM.  
     """
@@ -1607,7 +1609,7 @@ def calcNetEntropyTransfer(entropyTransfer):
 
     return netEntropyTransfer
 
-def overallNetEntropyTransfer(model):
+def calcOverallNetEntropyTransfer(model, turbo=False):
     """This function calculates the net entropy transfer for a whole structure 
     with a given time constant tau based on GNM.  
     """
@@ -1620,14 +1622,29 @@ def overallNetEntropyTransfer(model):
     n_atoms = model.numAtoms()
     n_modes = model.numModes()
 
-    tau_max = 20 
-    tau_step = 0.5
-    taus = np.arange(start=tau_step,stop=tau_max,step=tau_step)
-    numTaus = (tau_max / tau_step).astype(int)
+    tau_max = 5.0 
+    tau_step = 0.1
+    taus = np.arange(start=tau_step, stop=tau_max+1e-6, step=tau_step)
+    taus = np.insert(taus,0,0.000001)
+    numTaus = len(taus)
     netEntropyTransfer = np.zeros((numTaus,n_atoms,n_atoms))
-    LOGGER.timeit('_ent_trans')
-    for i in range(numTaus):
-        netEntropyTransfer[i,:,:]=calcNetEntropyTransfer(model,taus[i])
+    if turbo:
+        try:
+            from joblib import Parallel, delayed
+            import multiprocessing as mp
+        except: 
+            LOGGER.report('joblib and multiprocessing is not imported. Running' + 
+                'with sequential execution.')
+        LOGGER.timeit('_ent_trans')
+        n_cpu = mp.cpu_count()
+        netEntropyTransfer = Parallel(n_jobs=n_cpu)(delayed(calcAllEntropyTransfer)(model,taus[i]) \
+            for i in range(numTaus))
+        netEntropyTransfer = np.asarray(netEntropyTransfer)
+    
+    else:
+        LOGGER.timeit('_ent_trans')
+        for i in range(len(taus)):
+            netEntropyTransfer[i,:,:] = calcAllEntropyTransfer(model,taus[i])
 
     LOGGER.report('Net Entropy Transfer calculation is completed in %.1fs.',
                   '_ent_trans')
@@ -1644,7 +1661,24 @@ def overallNetEntropyTransfer(model):
 
     return overallNetEntropyTransfer
 
+def test():
+    from prody import parsePDB, GNM
+    from prody.dynamics.analysis import calcOverallNetEntropyTransfer
+    import matplotlib.pyplot as plt
 
+    pdb = parsePDB('1z83', subset='ca', chain='A')
+    gnm = GNM()
+    gnm.buildKirchhoff(pdb, cutoff=7.0)
+    gnm.calcModes(n_modes=None)
+    entTransfer = calcOverallNetEntropyTransfer(gnm,turbo=True)
+
+    # f = open('/data/Manuscript_data/Data/1Z83A/monomer_overallnet_A_cihan2.txt','w')
+    # for i in range(gnm.numAtoms()):
+    #     for j in range(gnm.numAtoms()):
+    #         if i != j:
+    #             f.write('%d\t%d\t%f\n' % (i+1,j+1,entTransfer[i,j]))
+    # f.close()
+    return entTransfer
 
 
 
