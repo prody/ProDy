47c4340d0cf8a95525e570b2de8704952ce24db6
shz66 <shz66@pitt.edu>
2018-03-16 13:03:53 -0400

    more tests

--- prody/tests/sequence/test_msa.py
+++ prody/tests/sequence/test_msa.py
@@ -107,6 +107,11 @@ class TestRefinement(TestCase):
         msa.extend(FASTA)
         assert_equal(msa[numSeq:].getArray(), FASTA.getArray(), 'MSA extension failed')
 
+        seqs = [str(seq) for seq in FASTA]
+        msa = FASTA[:]
+        msa.extend(FASTA)
+        assert_equal(msa[numSeq:].getArray(), FASTA.getArray(), 'MSA extension failed')
+
 class TestMerging(TestCase):
 
 
