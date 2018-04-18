38b4aad22801cc6348b1c3a1151150349fc6e6cd
shz66 <shz66@pitt.edu>
2018-04-17 22:53:42 -0400

    -

--- prody/tests/ensemble/test_functions.py
+++ prody/tests/ensemble/test_functions.py
@@ -30,11 +30,11 @@ class TestTrimPDBEnsemble(TestCase):
         weights[:, 0, :] = 0.
         ensemble.setWeights(weights)
         ensemble_trimed = trimPDBEnsemble(ensemble, occupancy=0.9)
-        assert_equal(calcOccupancies(ensemble_trimed).min() > 0.9,
+        assert(calcOccupancies(ensemble_trimed).min() > 0.9,
                      'soft trimPDBEnsemble failed')
         
         ensemble_trimed2 = trimPDBEnsemble(ensemble, occupancy=0.9, hard=True)
-        assert_equal(calcOccupancies(ensemble_trimed2).min() > 0.9,
+        assert(calcOccupancies(ensemble_trimed2).min() > 0.9,
                      'hard trimPDBEnsemble failed')
 
         atoms = ensemble_trimed.getAtoms()
