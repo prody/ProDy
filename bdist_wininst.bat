cee68b3b558b05009b4db894d6c1ca425aa695fd
shz66 <shz66@pitt.edu>
2018-04-27 10:38:06 -0400

    -

--- bdist_wininst.bat
+++ bdist_wininst.bat
@@ -1,5 +1,4 @@
 del /S /Q /F build\*.*
-C:\Python26\python.exe setup.py bdist_wininst
 C:\Python27\python.exe setup.py bdist_wininst
-C:\Python32\python.exe setup.py bdist_wininst
-C:\Python33\python.exe setup.py bdist_wininst
+C:\Python35\python.exe setup.py bdist_wininst
+C:\Python36\python.exe setup.py bdist_wininst
