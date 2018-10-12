f1cb8ed3ab49da84ee4fe82b0529ff5454d5abd9
luca <ponzoniluca@gmail.com>
2018-10-12 17:10:56 -0400

    rtb_domain.py module removed before push to official ProDy repo

--- prody/domain_decomposition/__init__.py
+++ prody/domain_decomposition/__init__.py
@@ -10,10 +10,6 @@ SETTINGS = prody.SETTINGS
 
 __all__ = []
 
-from . import rtb_domain
-__all__.extend(rtb_domain.__all__)
-from .rtb_domain import *
-
 from . import spectrus 
 __all__.extend(spectrus.__all__)
 from .spectrus import *
