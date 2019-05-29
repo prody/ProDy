1d493e03e0588b7fb7f6ea552d0923ef5ed2e50e
shz66 <shz66@pitt.edu>
2019-05-28 20:29:08 -0400

    debug

--- prody/sequence/msatools.c
+++ prody/sequence/msatools.c
@@ -1802,7 +1802,7 @@ static PyObject *msadirectinfo1(PyObject *self, PyObject *args, PyObject *kwargs
     #define prob(x,y) prob[(x)*q + (y)]
     #define align(x,y) align[(x)*l + (y)]
     printf("here\n");
-    printf("w[-1] = %d\n", w[number-1]);
+    printf("w[%d] = %f\n", number-1, w[number-1]);
     for (i = 0; i < number; i++)
         for (j = 0; j < l; j++)
             prob(j, align(i,j)) += pro_weight * w[i];
