5609ce32ff8e147522393bd5bb3aaff0066c655d
James Krieger <jamesmkrieger@gmail.com>
2019-07-30 14:36:57 -0400

    improvement again

--- prody/dynamics/plotting.py
+++ prody/dynamics/plotting.py
@@ -491,25 +491,28 @@ def showOverlapTable(modes_x, modes_y, **kwargs):
 
     if SETTINGS['auto_show']:
         plt.figure()
-    show = showMatrix(overlap, cmap=cmap, norm=norm, **kwargs)
     
-    x_range = np.arange(modes_x.numModes())
+    x_range = np.arange(1, modes_x.numModes()+1)
     if isinstance(modes_x, ModeSet):
-        x_ticklabels = modes_x._indices
+        x_ticklabels = modes_x._indices+1
     else:
         x_ticklabels = x_range
-    plt.xticks(x_range, x_ticklabels+1)
-    plt.xlabel(str(modes_x))
 
-    y_range = np.arange(modes_y.numModes())
+    x_ticklabels = kwargs.pop('xticklabels', x_ticklabels)
+
+    y_range = np.arange(1, modes_y.numModes()+1)
     if isinstance(modes_y, ModeSet):
-        y_ticklabels = modes_y._indices
+        y_ticklabels = modes_y._indices+1
     else:
         y_ticklabels = y_range
-    plt.yticks(y_range, y_ticklabels+1)
-    plt.ylabel(str(modes_y))
 
-    plt.minorticks_off()
+    y_ticklabels = kwargs.pop('yticklabels', y_ticklabels)
+
+    allticks = kwargs.pop('allticks', True)
+
+    show = showMatrix(overlap, cmap=cmap, norm=norm, 
+                      xticklabels=x_ticklabels, yticklabels=y_ticklabels, allticks=allticks,
+                      **kwargs)
     
     if SETTINGS['auto_show']:
         showFigure()
