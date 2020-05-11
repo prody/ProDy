d70134077d5baf7a86943a313e9cd63b04a83a45
James Krieger <jamesmkrieger@gmail.com>
2020-05-11 16:15:19 +0100

    changed to the subset option

--- prody/dynamics/signature.py
+++ prody/dynamics/signature.py
@@ -228,9 +228,12 @@ class ModeEnsemble(object):
 
         if index is None:
             return self._modesets
-        elif isinstance(index, Integral):
-            return self[index]
-        return self[index]._modesets
+
+        subset = self[index]
+        if isinstance(subset, ModeSet):
+            return subset
+        else:
+            return subset._modesets
 
     def getArray(self, mode_index=0):
         """Returns a sdarray of row arrays."""
