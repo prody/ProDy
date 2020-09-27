ff704704165e217341e325b5587bc5e2724fea60
James Krieger <jamesmkrieger@gmail.com>
2020-09-27 15:50:14 +0100

    improved CCD parsing

--- prody/compounds/ccd.py
+++ prody/compounds/ccd.py
@@ -7,76 +7,43 @@ for the Chemical Compound Dictionary (CCD_).
 
 import os.path
 
-from prody import LOGGER, SETTINGS, getPackagePath
+from prody import LOGGER, SETTINGS, getPackagePath, PY3K
 from prody.utilities import openFile, openURL, pystr, isListLike
-from prody.proteins import parseSTAR
+from prody.proteins.starfile import parseSTARLines, StarDict
 
-__all__ = ['fetchCCDviaHTTP', 'parseCCD']
+__all__ = ['parseCCD']
 
-url = 'http://ligand-expo.rcsb.org/dictionaries/Components-pub.cif'
-#ftp = 'ftp://ftp.wwpdb.org/pub/pdb/data/monomers/components.cif.gz'
-
-def fetchCCDviaHTTP(**kwargs):
+def parseCCD(ids):
     """Retrieve the whole Chemical Component Dictionary (CCD) resource.
     """
-    CCD_PATH = os.path.join(getPackagePath(), 'ccd')
-
-    success = 0
-    failure = 0
-    try:
-        handle = openURL(url)
-    except Exception as err:
-        LOGGER.warn('download failed ({1}).'.format(str(err)))
-        failure += 1
+    if isListLike(ids):
+        n_ids = len(ids)
     else:
-        data = handle.read()
-        if len(data):
-            if not os.path.isdir(CCD_PATH):
-                os.mkdir(CCD_PATH)
-            filename = CCD_PATH + '/Components-pub.cif'
+        ids = [ids]
+        n_ids = 1
 
-            with open(filename, 'w+b') as outfile:
-                outfile.write(data)
-
-            success += 1
-        else:
-            failure += 1
-    LOGGER.debug('CCD download via HTTP completed ({0} downloaded, '
-                 '{1} failed).'.format(success, failure))
-
-def parseCCD(*ids, **kwargs):
-    """"Parse data from the Chemical Component Dictionary (CCD) resource
-
-    :arg ids: one CCD identifier or a list of them.
-        If **None** is provided then all of them are returned.
-    :type ids: str, tuple, list, :class:`~numpy.ndarray`, **None**
-
-    Returns :class:`.StarDataBlock` object or list of them.
-    """
-    CCD_PATH = os.path.join(getPackagePath(), 'ccd')
-    
-    n_ids = len(ids)
-    if n_ids == 1:
-        if isListLike(ids[0]):
-            ids = ids[0]
-            n_ids = len(ids)
-
-    if n_ids == 1:
-        ids = list(ids)
-
-    filename = CCD_PATH + '/Components-pub.cif'
-    if not os.path.isfile(filename):
-        fetchCCDviaHTTP(**kwargs)
-
-    data = parseSTAR(filename, shlex=True)
     ret = []
     for id in ids:
+        id_url = 'http://ligand-expo.rcsb.org/reports/{0}/{1}/{1}.cif'.format(id[0],
+                                                                              id)
         try:
-            ret.append(data[id])
-        except ValueError:
-            LOGGER.warn('id {0} not found in CCD data '
-                        'so appending None'.format(id))
-            ret.append(None)
+            handle = openURL(id_url)
+        except Exception as err:
+            LOGGER.warn('download failed ({1}).'.format(str(err)))
+            failure += 1
+        else:
+            data = handle.read()
+            if len(data):
+                if PY3K:
+                    data = data.decode()
+
+                parsingDict, prog = parseSTARLines(data.split('\n'), shlex=True)
+                        
+                star_dict = StarDict(parsingDict, prog, id)
+                ret.append(star_dict[id])
+            else:
+                ret.append(None)
+                LOGGER.warn('Could not parse CCD data for {0}'.format(id))
 
     if n_ids == 1:
         return ret[0]
