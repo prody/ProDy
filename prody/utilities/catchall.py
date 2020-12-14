342bee5633863503548619e143698bf70b1bb65c
James Krieger <jamesmkrieger@gmail.com>
2020-12-14 18:24:33 +0000

    -

--- prody/utilities/catchall.py
+++ prody/utilities/catchall.py
@@ -67,7 +67,7 @@ def clusterSubfamilies(similarities, n_clusters=0, linkage='all', method='tsne',
 
     # Check inputs to make sure are of valid types/values
     if not isinstance(similarities, np.ndarray):
-        raise TypeError('similarities should be a a numpy ndarray')
+        raise TypeError('similarities should be a numpy ndarray')
 
     dim = similarities.shape
     if dim[0] != dim[1]:
