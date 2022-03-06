26a9c4384ada2f2e46a1ffff178dcaefb79bba85
James Krieger <jamesmkrieger@gmail.com>
2022-03-06 11:08:48 +0000

    exclude water

--- prody/proteins/compare.py
+++ prody/proteins/compare.py
@@ -336,7 +336,7 @@ class SimpleChain(object):
             aa = AAMAP.get(res.getResname(), 'X')
             if aa == '-':
                 aa = 'X'
-            if aa == 'X':
+            if aa == 'X' and res.getResname() != 'HOH':
                 LOGGER.warn("no one-letter mapping found for %s " % repr(res))
             if not res.getResname() in protein_resnames:
                 continue
