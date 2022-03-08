8666bd84e3b4d82d0fcb0ec7b07f0731010daec0
James Krieger <jamesmkrieger@gmail.com>
2022-03-08 17:38:45 +0000

    Revert "SimpleChain fix"
    This reverts commit f6d8d335c9b5d07c536c95b0f783e2c116f2f2c5.

--- prody/proteins/compare.py
+++ prody/proteins/compare.py
@@ -338,8 +338,6 @@ class SimpleChain(object):
                 aa = 'X'
             if aa == 'X':
                 LOGGER.warn("no one-letter mapping found for %s " % repr(res))
-            if not res.getResname() in protein_resnames:
-                continue
             simpres = SimpleResidue(self, i, resid, aa, incod, res)
             if gaps:
                 diff = resid - temp - 1
