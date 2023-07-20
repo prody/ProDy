e7032d7c084d2e769060359c5b81c86c8e23eac7
fdoljanin <doljaninfrane@gmail.com>
2023-07-20 13:19:55 +0200

    Memoization approach

--- prody/proteins/waterbridges.py
+++ prody/proteins/waterbridges.py
@@ -16,6 +16,9 @@ from prody import LOGGER
 from prody.measure import calcAngle
 from prody.measure.contacts import findNeighbors
 
+from timeit import default_timer as timer
+
+
 
 __all__ = ['calcWaterBridges']
 
@@ -71,8 +74,6 @@ def getWaterGroupAtoms(group):
 
     return uniqueAtomIndices
 
-def 
-
 def calcBridgesFromHBonds_cluster(hydrogenBonds):
     waterBridges = []
 
@@ -113,33 +114,70 @@ def listToSmallerDim(list):
     
     return newList
 
-def goDeeper(currentBond, hydrogenBondsWithDepth, maxDepth):
+def goDeeper_old(currentBond, hydrogenBondsWithDepth, maxDepth):
     if currentBond[2] != 0 and ResType.PROTEIN in [currentBond[0].type, currentBond[1].type]:
         return [[currentBond]]
 
     if currentBond[2] == maxDepth:
         return [[]]
-    
+
     newBonds = []
     for bond in hydrogenBondsWithDepth:
         if bond[2] == None and (currentBond[0].hydrophilic.getIndex() in [bond[0].hydrophilic.getIndex(), bond[1].hydrophilic.getIndex()] or currentBond[1].hydrophilic.getIndex() in [bond[0].hydrophilic.getIndex(), bond[1].hydrophilic.getIndex()]):
             bond[2] = currentBond[2] + 1
             newBonds.append(bond)
-    
+
     newBondsChains = listToSmallerDim([goDeeper(bond, hydrogenBondsWithDepth, maxDepth) for bond in newBonds])
     return [[currentBond, *chain] for chain in newBondsChains if chain]
     
-
-
-def calcBridgesFromHBonds_two(hydrogenBonds, hydrophilic):
+def calcBridgesFromHBonds_depth_old(hydrogenBonds, hydrophilic):
     hydrogenBondsWithDepth = [[hb[0], hb[1], None] for hb in hydrogenBonds]
     initialBonds = []
     for bond in hydrogenBonds:
       if hydrophilic.getIndex() in [bond[0].hydrophilic.getIndex(), bond[1].hydrophilic.getIndex()]:
           initialBonds.append([bond[0], bond[1], 0])
 
-    newBondsChains = listToSmallerDim([goDeeper(bond, hydrogenBondsWithDepth, 3) for bond in initialBonds])
+    newBondsChains = listToSmallerDim([goDeeper(bond, hydrogenBondsWithDepth, 2) for bond in initialBonds])
     return newBondsChains
+
+def getRelationsList(hydrogenBonds, numAtoms):
+    relations = [[None, [], [], False] for _ in range(numAtoms)]
+    for bond in hydrogenBonds:
+        index_0, index_1 = bond[0].hydrophilic.getIndex(), bond[1].hydrophilic.getIndex()
+        relations[index_0][0] = bond[0]
+        relations[index_1][0] = bond[1]
+
+        relations[index_0][1].append(bond)
+        relations[index_1][1].append(bond)
+        
+        relations[index_0][2].append(index_1)
+        relations[index_1][2].append(index_0)
+
+    return relations
+    
+def goDeeper(currentAtom, relationsList, depthLeft):
+    currentIndex = currentAtom.hydrophilic.getIndex()
+    relationsList[currentIndex] = True
+
+    if currentAtom.type == ResType.PROTEIN:
+        return [[currentAtom]]
+    
+    if depthLeft == 0:
+        return [[]]
+    
+    newAtoms = []
+    for atomIndex in relationsList[currentIndex][2]:
+        if not relationsList[atomIndex][3]:
+            newAtoms.append(relationsList[atomIndex][0])
+    
+    newAtomsChains = listToSmallerDim([goDeeper(atom, relationsList, depthLeft-1) for atom in newAtoms])
+    return [[currentAtom, *chain] for chain in newAtomsChains if chain]
+    
+def calcBridgesFromHBonds(watchedAtom, relationsList, depth):
+    initialAtom = relationsList[watchedAtom[0].hydrophilic.getIndex()]
+    initialAtom[2] = True
+    
+    return goDeeper(initialAtom, relationsList, depth-1)
 
 
 def calcWaterBridges(atoms, **kwargs):
@@ -152,9 +190,9 @@ def calcWaterBridges(atoms, **kwargs):
         default is 3.5
     :type distDA: int, float
 
-    :arg distPR: maximal distance between water and residue
+    :arg distWR: maximal distance between water and residue
         default is 4
-    :type distPR: int, float
+    :type distWR: int, float
 
     :arg anglePDWA: angle range where protein is donor and water is acceptor
         default is (100, 200)
@@ -178,14 +216,14 @@ def calcWaterBridges(atoms, **kwargs):
     """
 
     distDA = kwargs.pop('distDA', 3.5)
-    distPR = kwargs.pop('distPR', 4)
+    distWR = kwargs.pop('distWR', 4)
     anglePDWA = kwargs.pop('anglePDWA', (100, 200))
     anglePAWD = kwargs.pop('anglePAWD', (100, 140))
     angleWW = kwargs.pop('angleWW', (140, 180))
     donors = kwargs.pop('donors', ['nitrogen', 'oxygen', 'sulfur'])
     acceptors = kwargs.pop('acceptors', ['nitrogen', 'oxygen', 'sulfur'])
 
-    consideredAtoms = ~atoms.select(f'water and not within {distPR} of protein')
+    consideredAtoms = ~atoms.select(f'water and not within {distWR} of protein')
     waterHydrogens = consideredAtoms.select('water and hydrogen')
     waterOxygens = consideredAtoms.select('water and oxygen')
     neighborWaterOxygens = findNeighbors(waterOxygens, distDA)
@@ -234,8 +272,11 @@ def calcWaterBridges(atoms, **kwargs):
     LOGGER.info(f'Hydrogen bonds ({len(hydrogenBonds)}) calculated.')
 
     #waterBridges = calcBridgesFromHBonds_cluster(hydrogenBonds)
-    for i in range(100):
-      waterBridges = calcBridgesFromHBonds_two(hydrogenBonds, atoms[2459])
+    start = timer()
+    relations = getRelationsList(hydrogenBonds, len(atoms))
+    waterBridges = calcBridgesFromHBonds(relations[2459], relations, 2)
+    end = timer()
+    LOGGER.info(end-start)
     LOGGER.info(f'Water bridges calculated.')
 
     return waterBridges
