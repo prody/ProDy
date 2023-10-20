6a78591f55982b32b4073953595f3eab10db6631
James Krieger <jamesmkrieger@gmail.com>
2023-10-20 14:05:16 +0100

    fix clustenm app and update datafiles

--- prody/tests/datafiles/__init__.py
+++ prody/tests/datafiles/__init__.py
@@ -315,6 +315,12 @@ DATA_FILES = {
     '2k39_all2': {
         'file': '2k39_all2.npy'
     },
+    'pdb4ake_fixed': {
+        'file': '4akeA_alg_fixed.pdb'
+    },
+    'mrc1ake': {
+        'file': '1ake.mrc'
+    },
 }
 
 
