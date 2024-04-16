78cd7f2644bf5f548ca192aae2f3a013ffd8da63
James Krieger <jamesmkrieger@gmail.com>
2024-04-16 13:48:00 +0200

    account for input proj

--- prody/dynamics/plotting.py
+++ prody/dynamics/plotting.py
@@ -275,7 +275,7 @@ def showProjection(ensemble=None, modes=None, projection=None, *args, **kwargs):
 
     use_labels = kwargs.pop('use_labels', True)
     labels = kwargs.pop('label', None)
-    if labels is None and  use_labels:
+    if labels is None and  use_labels and modes is not None:
         if isinstance(modes, (LDA, LRA)):
             labels = modes._labels.tolist()
             LOGGER.info('using labels from LDA modes')
