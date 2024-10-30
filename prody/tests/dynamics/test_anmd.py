fbde00673c352e7ca72bd947268ea09b1fec459e
James Krieger <jamesmkrieger@gmail.com>
2024-10-30 08:03:32 +0000

    -

--- prody/tests/dynamics/test_anmd.py
+++ prody/tests/dynamics/test_anmd.py
@@ -84,7 +84,7 @@ class TestAnmdResults(TestANMD):
 
             ens1.setCoords(ens1.getCoordsets()[2])
             assert_allclose(ens1.getRMSDs(), ENSEMBLE.getRMSDs(), 
-                            rtol=1e-10, atol=0.1, # may not be so
+                            rtol=1e-10, atol=0.1, # may not be so close
                             err_msg='runANMD with default parameters failed to give expected RMSDs')
 
     def testResultsNumModes1(self):
@@ -100,7 +100,7 @@ class TestAnmdResults(TestANMD):
 
             ens1.setCoords(ens1.getCoordsets()[2])
             assert_allclose(ens1.getRMSDs(), ENSEMBLE.getRMSDs(), 
-                            rtol=1e-10, atol=0.1, # may not be so
+                            rtol=1e-10, atol=0.1, # may not be so close
                             err_msg='runANMD with num_modes=1 failed to give expected RMSDs')
 
 if __name__ == '__main__':
