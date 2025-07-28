6894736143ef21a80eae225300b310a203a6c535
Vishal Vyamajala <vsv12mar@gmail.com>
2025-07-28 18:29:56 -0400

    Prody 2.6.0

--- setup.py
+++ setup.py
@@ -122,13 +122,13 @@ EXTENSIONS = [
 #              glob(join('prody', 'dynamics', 'saxstools.c')),
 #              include_dirs=[numpy.get_include()]),
     Extension('prody.sequence.msatools',
-              [join('prody', 'sequence', 'msatools.c'),],
+              [join('prody', 'sequence', 'msatools.c')],
               include_dirs=[numpy.get_include()]),
     Extension('prody.sequence.msaio',
-              [join('prody', 'sequence', 'msaio.c'),],
+              [join('prody', 'sequence', 'msaio.c')],
               include_dirs=[numpy.get_include()]),
     Extension('prody.sequence.seqtools',
-              [join('prody', 'sequence', 'seqtools.c'),],
+              [join('prody', 'sequence', 'seqtools.c')],
               include_dirs=[numpy.get_include()]),
 ]
 
@@ -138,7 +138,6 @@ if platform.system() == 'Darwin':
     os.environ['MACOSX_DEPLOYMENT_TARGET'] = os_ver
     os.environ['CC'] = 'clang'
     os.environ['CXX'] = 'clang++'
-    #extra_compile_args.append('-stdlib=libc++')
 
 CONTRIBUTED = [
     Extension('prody.kdtree._CKDTree',
@@ -154,12 +153,17 @@ for ext in CONTRIBUTED:
     if all([isfile(src) for src in ext.sources]):
         EXTENSIONS.append(ext)
 
-# SCRIPTS = ['scripts/prody', 'scripts/evol']
-# if (platform.system() == 'Windows' or
-#     len(sys.argv) > 1 and sys.argv[1] not in ('build', 'install')):
-#     for script in list(SCRIPTS):
-#         SCRIPTS.append(script + '.bat')
+hpb_cpp = join('prody', 'proteins', 'hpbmodule', 'hpbmodule.cpp')
+hpb_f   = join('prody', 'proteins', 'hpbmodule', 'reg_tet.f')
 
+if isfile(hpb_cpp) and isfile(hpb_f):
+    EXTENSIONS.append(
+        Extension('prody.proteins.hpb',
+                  sources=[hpb_cpp, hpb_f],
+                  include_dirs=[numpy.get_include()],
+                  language='c++',
+                  extra_compile_args=['-O3', '-fPIC'])
+    )
 
 SCRIPTS = ['prody=prody.apps:prody_main', 'evol=prody.apps:evol_main']
 
@@ -181,21 +185,19 @@ setup(
               'essential dynamics analysis, principal component analysis, '
               'Protein Data Bank, PDB, GNM, ANM, SM, PCA'),
     classifiers=[
-                 'Development Status :: 5 - Production/Stable',
-                 'Intended Audience :: Education',
-                 'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: MIT License',
-                 'Operating System :: MacOS',
-                 'Operating System :: POSIX',
-                 'Programming Language :: Python',
-                 'Programming Language :: Python :: 3',
-                 'Topic :: Scientific/Engineering :: Bio-Informatics',
-                 'Topic :: Scientific/Engineering :: Chemistry',
-                ],
-    #scripts=SCRIPTS,
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+    ],
     entry_points = {
         'console_scripts': SCRIPTS,
     },
     install_requires=INSTALL_REQUIRES,
-    #provides=['ProDy ({0:s})'.format(__version__)]
 )
