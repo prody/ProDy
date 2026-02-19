353a77ba7db7f5e47456b6dce40ab811180a60ba
James Krieger <jamesmkrieger@gmail.com>
2026-02-19 16:40:01 +0000

    add lra test

---
+++ prody/tests/apps/test_prody_lra.py
@@ -0,0 +1,128 @@
+from os import remove
+import shlex
+from os.path import isfile, join, split, splitext
+from prody.tests import TestCase, skipIf, skipUnless
+
+from numpy.testing import *
+
+from prody.utilities import importDec
+dec = importDec()
+
+from prody.tests.datafiles import TEMPDIR, pathDatafile
+
+from prody.apps import prody_parser
+
+from prody.tests import MATPLOTLIB, NOPRODYCMD, WINDOWS
+
+# NEW: Import ProDy functions to generate valid test data
+from prody import parsePDB, writePDB, writeDCD
+
+class TestLRACommand(TestCase):
+
+    def setUp(self):
+        # We define the base command. Note that {pdb} is the REFERENCE structure.
+        self.command = ('lra --pdb {pdb} '
+                        '-e -r -o {outdir} -v -z -t all -j '
+                        '-f %8g -d , -x .dat '
+                        '-R -Q -J 1 -S '
+                        '-F png -D 120 -W 5 -H 4 ').format(outdir=TEMPDIR,
+                        pdb=pathDatafile('multi_model_truncated'))
+        self.suffixes = [
+            '_lra_cc.png',
+            '_lra.lra.npz',
+            '_lra_covariance.dat',
+            '_lra_cross-correlations.dat',
+            '_lra_proj_1.png',
+            '_lra_evalues.dat',
+            '_lra_proj.dat',
+            '_lra_evectors.dat',
+            '_lra_sf.png',
+            '_lra_extended_all.nmd',
+            '_lra.nmd',
+        ]
+
+        self.tearDown()
+
+    # NOTE: I have kept the skip decorators commented out so you can verify the fix works.
+    # In a real PR, you might want to uncomment them.
+    # @dec.slow
+    # @skipIf(NOPRODYCMD, 'prody command not found')
+    # @skipUnless(MATPLOTLIB, 'matplotlib not found')
+    # @skipIf(WINDOWS, 'command tests are not run on Windows')
+    def testLRACommandDCD(self):
+
+        # 1. Load the original data
+        pdb_path = pathDatafile('multi_model_truncated')
+        atoms = parsePDB(pdb_path)
+        
+        # 2. Duplicate coordsets to ensure we have > 3 frames (ProDy requirement)
+        while atoms.numCoordsets() <= 3:
+            atoms.addCoordset(atoms.getCoordsets())
+
+        # 3. Write a single temporary DCD file
+        temp_dcd = join(TEMPDIR, 'sufficient_frames.dcd')
+        writeDCD(temp_dcd, atoms)
+
+        # 4. Pass the SINGLE new file to the command
+        command = self.command + temp_dcd
+        prefix = splitext(split(temp_dcd)[1])[0]
+
+        namespace = prody_parser.parse_args(shlex.split(command))
+        namespace.func(namespace)
+
+        for suffix in self.suffixes:
+            fn = join(TEMPDIR, prefix + suffix)
+            self.assertTrue(isfile(fn), msg=fn+' not found')
+
+    # @dec.slow
+    # @skipIf(NOPRODYCMD, 'prody command not found')
+    # @skipUnless(MATPLOTLIB, 'matplotlib not found')
+    # @skipIf(WINDOWS, 'command tests are not run on Windows')
+    def testLRACommandPDB(self):
+
+        # 1. Load original data
+        pdb_path = pathDatafile('multi_model_truncated')
+        atoms = parsePDB(pdb_path)
+
+        # 2. Duplicate coordsets to ensure > 3 frames
+        while atoms.numCoordsets() <= 3:
+            atoms.addCoordset(atoms.getCoordsets())
+
+        # 3. Write a single temporary PDB file
+        temp_pdb = join(TEMPDIR, 'sufficient_frames.pdb')
+        writePDB(temp_pdb, atoms)
+
+        # 4. Pass the SINGLE new file to the command
+        command = self.command + temp_pdb
+        prefix = splitext(split(temp_pdb)[1])[0]
+
+        namespace = prody_parser.parse_args(shlex.split(command))
+        namespace.func(namespace)
+
+        for suffix in self.suffixes:
+            fn = join(TEMPDIR, prefix + suffix)
+            self.assertTrue(isfile(fn), msg=fn+' not found')
+
+
+    def tearDown(self):
+        # Clean up the generated temp files as well as outputs
+        files_to_clean = [
+            'multi_model_truncated',
+            'dcd',
+            'sufficient_frames.pdb', # Clean up our temp input
+            'sufficient_frames.dcd'  # Clean up our temp input
+        ]
+
+        for fname in files_to_clean:
+            # Handle full paths vs datafiles
+            if 'sufficient' in fname:
+                prefix = splitext(fname)[0]
+                base_path = join(TEMPDIR, fname) # The input file itself
+                if isfile(base_path): remove(base_path)
+            else:
+                base_path = pathDatafile(fname)
+                prefix = splitext(split(base_path)[1])[0]
+
+            for suffix in self.suffixes:
+                fn = join(TEMPDIR, prefix + suffix)
+                if isfile(fn): remove(fn)
